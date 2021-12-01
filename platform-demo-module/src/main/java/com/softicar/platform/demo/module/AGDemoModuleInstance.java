9c29c0ec2f32ec5ed72a28acb96a2dacc999dd95
Oliver Richers <oliver.richers@preventgroup.com>
2021-12-01 14:40:33 +0100

    PLAT-580 Implemented Test for Module Instance Creation

--- platform-demo-module/src/main/java/com/softicar/platform/demo/module/AGDemoModuleInstance.java
+++ platform-demo-module/src/main/java/com/softicar/platform/demo/module/AGDemoModuleInstance.java
@@ -1,8 +1,18 @@
 package com.softicar.platform.demo.module;
 
+import com.softicar.platform.common.core.i18n.IDisplayString;
 import com.softicar.platform.core.module.module.instance.standard.IStandardModuleInstance;
 
 public class AGDemoModuleInstance extends AGDemoModuleInstanceGenerated implements IStandardModuleInstance<AGDemoModuleInstance> {
 
-	// add custom code here
+	@Override
+	public IDisplayString toDisplayWithoutId() {
+
+		String title = getTitle();
+		if (title.isBlank()) {
+			return IStandardModuleInstance.super.toDisplayWithoutId();
+		} else {
+			return IDisplayString.create(title);
+		}
+	}
 }
--- platform-demo-module/src/test/java/com/softicar/platform/demo/module/AbstractDemoModuleTest.java
+++ platform-demo-module/src/test/java/com/softicar/platform/demo/module/AbstractDemoModuleTest.java
@@ -1,22 +1,15 @@
 package com.softicar.platform.demo.module;
 
-import com.softicar.platform.core.module.user.AGUser;
-import com.softicar.platform.core.module.user.CurrentUser;
-import com.softicar.platform.db.runtime.test.AbstractDbTest;
+import com.softicar.platform.core.module.test.AbstractCoreTest;
 import com.softicar.platform.demo.module.test.fixture.DemoModuleTestFixtureMethods;
+import com.softicar.platform.emf.test.IEmfTestEngineMethods;
 
-public abstract class AbstractDemoModuleTest extends AbstractDbTest implements DemoModuleTestFixtureMethods {
+public abstract class AbstractDemoModuleTest extends AbstractCoreTest implements DemoModuleTestFixtureMethods, IEmfTestEngineMethods {
 
-	protected final AGUser user;
 	protected final AGDemoModuleInstance moduleInstance;
 
 	public AbstractDemoModuleTest() {
 
-		this.user = insertUser("Test User")//
-			.setEmailAddress("user@example.com")
-			.save();
 		this.moduleInstance = insertDemoModuleInstance();
-
-		CurrentUser.set(user);
 	}
 }
