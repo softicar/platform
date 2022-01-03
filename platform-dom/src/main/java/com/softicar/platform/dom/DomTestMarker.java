985f721304f7eeb2c0510896f5ec5c07fb035817
Oliver Richers <oliver.richers@preventgroup.com>
2022-01-03 18:46:45 +0100

    renamed DomTestMarker.HOURS_INPUT

--- platform-dom/src/main/java/com/softicar/platform/dom/DomTestMarker.java
+++ platform-dom/src/main/java/com/softicar/platform/dom/DomTestMarker.java
@@ -4,7 +4,7 @@ import com.softicar.platform.common.core.interfaces.IStaticObject;
 
 public enum DomTestMarker implements IStaticObject {
 	DAY_INPUT,
-	HOUR_INPUT,
+	HOURS_INPUT,
 	MINUTES_INPUT,
 	SECONDS_INPUT,
 }
