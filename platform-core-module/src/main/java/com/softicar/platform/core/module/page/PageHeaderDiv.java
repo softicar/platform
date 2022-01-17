5bc8885e5ed4fe787af072b04df6f0983f602c63
Daniel Klose <daniel.klose@forspace-solutions.com>
2022-01-17 14:33:50 +0100

    PLAT-641 Change URL when Logging out

--- platform-core-module/src/main/java/com/softicar/platform/core/module/page/PageHeaderDiv.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/page/PageHeaderDiv.java
@@ -8,7 +8,9 @@ import com.softicar.platform.core.module.CoreI18n;
 import com.softicar.platform.core.module.CoreImages;
 import com.softicar.platform.core.module.page.navigation.PageNavigationToggleButton;
 import com.softicar.platform.core.module.page.navigation.link.PageNavigationLink;
+import com.softicar.platform.core.module.page.service.PageService;
 import com.softicar.platform.core.module.user.CurrentUser;
+import com.softicar.platform.core.module.web.service.WebServiceUrlBuilder;
 import com.softicar.platform.dom.document.CurrentDomDocument;
 import com.softicar.platform.dom.elements.DomDiv;
 import com.softicar.platform.dom.elements.button.DomButton;
@@ -93,6 +95,7 @@ class PageHeaderDiv<I extends IEmfModuleInstance<I>> extends DomDiv {
 		public void logout() {
 
 			getAjaxRequest().getHttpSession().invalidate();
+			getDomEngine().pushBrowserHistoryState(CoreI18n.LOGIN.toString(), new WebServiceUrlBuilder(PageService.class).build().getStartingFromPath());
 			getDomEngine().reloadPage();
 		}
 	}
