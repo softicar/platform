25ffab2610a24ae148e08e5d79ea33ca9c3f0817
Daniel Klose <daniel.klose@forspace-solutions.com>
2022-01-26 11:35:21 +0100

    javadoc improvement

--- platform-core-module/src/main/java/com/softicar/platform/core/module/file/smb/ISmbClient.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/file/smb/ISmbClient.java
@@ -1,5 +1,7 @@
 package com.softicar.platform.core.module.file.smb;
 
+import java.io.OutputStream;
+
 /**
  * Common interface of all SMB client implementations.
  *
@@ -17,6 +19,11 @@ public interface ISmbClient {
 	/**
 	 * Creates a new {@link ISmbFile} from the given URL and
 	 * {@link SmbCredentials}.
+	 * <p>
+	 * Note that if the file at the given URL does not exist, it won't get
+	 * physically created until until you create an {@link OutputStream} via
+	 * {@link ISmbFile#createOutputStream()} and write the corresponding data to
+	 * it.
 	 *
 	 * @param url
 	 *            the URL to the file on the SMB share (never <i>null</i>)
@@ -29,9 +36,12 @@ public interface ISmbClient {
 	/**
 	 * Creates a new {@link ISmbDirectory} from the given URL and
 	 * {@link SmbCredentials}.
+	 * <p>
+	 * If the directory at the given URL does not exist, you need to physically
+	 * create it using {@link ISmbDirectory#mkdirs()}.
 	 *
 	 * @param url
-	 *            the URL to the file on the SMB share (never <i>null</i>)
+	 *            the URL to the directory on the SMB share (never <i>null</i>)
 	 * @param credentials
 	 *            the credentials to log in to the SMB share (never <i>null</i>)
 	 * @return the new {@link ISmbDirectory} (never <i>null</i>)
