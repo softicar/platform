1bdeaa1bff82d169fc5d31af14fa78456ef1c0c4
Thees Koester <thees.koester@forspace-solutions.com>
2022-01-28 06:03:10 +0100

    PLAT-648 Delete Old Program Executions Automatically (#105)
    * PLAT-610 Delete Old Program Executions Automatically
    
    * Add ProgramExecutionCleanupProgram
    
    Add new column "retentionDaysOfExecutions" to AGProgram
    
    * default value is 28
    
    PLAT-632 Fix Password Input Eye Alignment (#91)
    
    PLAT-625 Change default FROM address in user notification (#92)
    
    PLAT-638 DomButton: Use ellipsis for long button labels (#95)
    
    PLAT-637 Show Password in Popup after reset (#93)
    
    * PLAT-637 Show Password in Popup after reset
    
    * sorted I18n
    
    * adapted text and style
    
    * only wrap text for alert, confirm and prompt modal
    
    PLAT-639 Fix ScheduledProgramEnqueuer skipping executions (#97)
    
    Sort members of CoreI18n
    
    Make retentionDays editable and add ProgramValidator
    
    Delete programExecutions according to the same reference day
    
    Fixed deletion statements in ProgramExecutionCleanupProgram
    
    Add ProgramExecutionCleanupProgramTest
    
    Outsource deletion action to ProgramExecutionDeleter
    
    * Fix duplicate method in ScheduledProgramEnqueuerTest
    
    * Fix bug in customizeAttributeProperties() of AGProgramTable
    
    * Fix error in ProgramExecutionDeleterTest insertProgramExecutionWithLog()
    
    * Workaround for error of AGProgram's toDisplay() in JUnit test
    
    * Split up methods of ProgramExecutionDeleterTest
    
    * Add logging of new column "retentionDaysOfExecution"
    
    * ProgramExecutionDeleterTest extends AbstractCoreTest
    
    * Use TestClock for Junit test and rename methods
    
    * Changes according to review.
    
    * Improve ProgramExecutionsDeleterTest
    
    * Should be easier to read now
    * Has several test methods to validate different scenarios
    
    * Add method testDeleteWithProgramWithRetentionDaysOfExecutionsOfThree()
    
    * Create reused methods and change order of methods
    
    * Fix issue loading the program name in JUnit test
    
    * Remove method fetchProgramNameIfPossible()
    * Add and use three TestProgram classes in JUnit test
    
    * Add AbstractTestProgram
    
    * Reduce visibility of AbstractTestProgram to "private"
    
    * Remove enum ExecutionDay, use constants instead
    
    * Rename "retention days of executions" to "execution retention days"
    
    * Don't use expectedRetainedProgramExecutionsMap anymore

--- platform-core-module/src/main/java/com/softicar/platform/core/module/program/AGProgramGenerated.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/program/AGProgramGenerated.java
@@ -5,6 +5,7 @@ import com.softicar.platform.core.module.CoreI18n;
 import com.softicar.platform.core.module.uuid.AGUuid;
 import com.softicar.platform.db.runtime.field.IDbForeignField;
 import com.softicar.platform.db.runtime.field.IDbIdField;
+import com.softicar.platform.db.runtime.field.IDbIntegerField;
 import com.softicar.platform.db.runtime.key.IDbKey;
 import com.softicar.platform.db.runtime.logic.AbstractDbObject;
 import com.softicar.platform.db.runtime.object.DbObjectTableBuilder;
@@ -29,6 +30,7 @@ public class AGProgramGenerated extends AbstractDbObject<AGProgram> {
 
 	public static final IDbIdField<AGProgram> ID = BUILDER.addIdFieldForLong("id", o->o.m_id, (o,v)->o.m_id=v).setTitle(CoreI18n.ID);
 	public static final IDbForeignField<AGProgram, AGUuid> PROGRAM_UUID = BUILDER.addForeignField("programUuid", o->o.m_programUuid, (o,v)->o.m_programUuid=v, AGUuid.ID).setTitle(CoreI18n.PROGRAM_UUID);
+	public static final IDbIntegerField<AGProgram> EXECUTION_RETENTION_DAYS = BUILDER.addIntegerField("executionRetentionDays", o->o.m_executionRetentionDays, (o,v)->o.m_executionRetentionDays=v).setTitle(CoreI18n.EXECUTION_RETENTION_DAYS).setDefault(28);
 	public static final IDbKey<AGProgram> UK_PROGRAM_UUID = BUILDER.addUniqueKey("programUuid", PROGRAM_UUID);
 	public static final AGProgramTable TABLE = new AGProgramTable(BUILDER);
 	// @formatter:on
@@ -70,6 +72,16 @@ public class AGProgramGenerated extends AbstractDbObject<AGProgram> {
 		return setValue(PROGRAM_UUID, value);
 	}
 
+	public final Integer getExecutionRetentionDays() {
+
+		return getValue(EXECUTION_RETENTION_DAYS);
+	}
+
+	public final AGProgram setExecutionRetentionDays(Integer value) {
+
+		return setValue(EXECUTION_RETENTION_DAYS, value);
+	}
+
 	// -------------------------------- UTILS -------------------------------- //
 
 	@Override
@@ -82,5 +94,6 @@ public class AGProgramGenerated extends AbstractDbObject<AGProgram> {
 
 	private Integer m_id;
 	private AGUuid m_programUuid;
+	private Integer m_executionRetentionDays;
 }
 
