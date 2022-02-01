466814f93e61b834a9d1941cacd02e16271f3524
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-02-01 20:53:34 +0100

    PLAT-669 Open entity popups from table-row display elements
    Changed `EmfTableRowDisplay` to display a popup button for the referenced entity, instead of plain text. This allows for "following an FK".

--- platform-emf/src/main/java/com/softicar/platform/emf/table/row/EmfTableRowDisplay.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/table/row/EmfTableRowDisplay.java
@@ -1,13 +1,40 @@
 package com.softicar.platform.emf.table.row;
 
+import com.softicar.platform.common.core.i18n.IDisplayString;
 import com.softicar.platform.dom.elements.DomDiv;
+import com.softicar.platform.dom.elements.button.DomButton;
+import com.softicar.platform.dom.elements.popup.manager.DomPopupManager;
+import com.softicar.platform.emf.EmfCssClasses;
+import com.softicar.platform.emf.EmfI18n;
+import com.softicar.platform.emf.form.popup.EmfFormPopup;
 
 public class EmfTableRowDisplay<R extends IEmfTableRow<R, ?>> extends DomDiv {
 
 	public EmfTableRowDisplay(R row) {
 
+		setCssClass(EmfCssClasses.EMF_TABLE_ROW_DISPLAY);
+
 		if (row != null) {
-			appendText(row.toDisplay());
+			appendChild(new ViewButton(row));
+		}
+	}
+
+	private class ViewButton extends DomButton {
+
+		public ViewButton(R row) {
+
+			IDisplayString label = row.toDisplay();
+			setLabel(label);
+			setTitle(EmfI18n.VIEW.concatColon().concatSpace().concat(label));
+			setClickCallback(() -> showEntityFormPopup(row));
+		}
+
+		private void showEntityFormPopup(R row) {
+
+			DomPopupManager//
+				.getInstance()
+				.getPopup(row, EmfFormPopup.class, EmfFormPopup::new)
+				.show();
 		}
 	}
 }
