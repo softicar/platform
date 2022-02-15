b49f1e9e63980a4938e289ab108ef425b174e99e
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-02-15 14:10:21 +0100

    PLAT-581-2 Basic CSV-Import for EmfManagementDiv (#143)

---
+++ platform-demo-module/src/test/java/com/softicar/platform/demo/module/invoice/importing/DemoInvoiceImportTest.java
@@ -0,0 +1,72 @@
+package com.softicar.platform.demo.module.invoice.importing;
+
+import com.softicar.platform.common.string.charset.Charsets;
+import com.softicar.platform.demo.module.AbstractDemoModuleTest;
+import com.softicar.platform.demo.module.invoice.AGDemoInvoice;
+import com.softicar.platform.dom.event.upload.IDomFileUpload;
+import com.softicar.platform.dom.event.upload.IDomFileUploadHandler;
+import com.softicar.platform.emf.EmfI18n;
+import com.softicar.platform.emf.management.EmfManagementDivBuilder;
+import com.softicar.platform.emf.management.EmfManagementMarker;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+import org.junit.Test;
+
+public class DemoInvoiceImportTest extends AbstractDemoModuleTest {
+
+	public DemoInvoiceImportTest() {
+
+		setNodeSupplier(() -> new EmfManagementDivBuilder<>(AGDemoInvoice.TABLE, moduleInstance).build());
+	}
+
+	@Test
+	public void testNormalImport() {
+
+		findButton(EmfManagementMarker.IMPORT_BUTTON).click();
+
+		var popup = findPopup(EmfManagementMarker.IMPORT_POPUP);
+		popup//
+			.findNodes()
+			.filter(IDomFileUploadHandler.class::isInstance)
+			.assertOne(node -> IDomFileUploadHandler.class.cast(node))
+			.handleFileUploads(List.of(new DummyUpload("foo.csv", "123,2022-02-02,0,,\n234,2022-02-10,1,,")));
+		popup.clickNode(EmfI18n.ANALYZE);
+		popup.clickNode(EmfI18n.SAVE_AND_CLOSE);
+
+		var table = findEmfDataTable(AGDemoInvoice.TABLE);
+		assertEquals("123|234", table.getTextInCells(AGDemoInvoice.INVOICE_NUMBER));
+		assertEquals("2022-02-02|2022-02-10", table.getTextInCells(AGDemoInvoice.INVOICE_DATE));
+		assertEquals("No|Yes", table.getTextInCells(AGDemoInvoice.LOCKED_ITEMS));
+	}
+
+	private static class DummyUpload implements IDomFileUpload {
+
+		private final String filename;
+		private final String content;
+
+		public DummyUpload(String filename, String content) {
+
+			this.filename = filename;
+			this.content = content;
+		}
+
+		@Override
+		public String getFilename() {
+
+			return filename;
+		}
+
+		@Override
+		public InputStream getStream() {
+
+			return new ByteArrayInputStream(content.getBytes(Charsets.UTF8));
+		}
+
+		@Override
+		public String getContentType() {
+
+			return "";
+		}
+	}
+}
