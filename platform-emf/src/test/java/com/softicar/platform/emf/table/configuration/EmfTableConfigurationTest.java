501d265f8551978671b7bfc603115fbaee1bd334
Thees Koester <thees.koester@forspace-solutions.com>
2022-02-18 15:27:33 +0100

    PLAT-589 CSV Import: Option to set business key of EmfTables (#151)

---
+++ platform-emf/src/test/java/com/softicar/platform/emf/table/configuration/EmfTableConfigurationTest.java
@@ -0,0 +1,30 @@
+package com.softicar.platform.emf.table.configuration;
+
+import com.softicar.platform.db.runtime.test.AbstractDbTest;
+import com.softicar.platform.emf.test.module.EmfTestModuleInstance;
+import com.softicar.platform.emf.test.simple.EmfTestObject;
+import org.junit.Test;
+
+public class EmfTableConfigurationTest extends AbstractDbTest {
+
+	private final EmfTableConfiguration<EmfTestObject, Integer, EmfTestModuleInstance> configuration;
+
+	public EmfTableConfigurationTest() {
+
+		configuration = new EmfTableConfiguration<>(EmfTestObject.TABLE);
+	}
+
+	@Test
+	public void testGetBusinessKeyWithDefaultValue() {
+
+		assertTrue(configuration.getBusinessKey().isPrimaryKey());
+	}
+
+	@Test
+	public void testGetBusinessKeyWithNonDefaultValue() {
+
+		configuration.setBusinessKey(EmfTestObject.UK_NAME_DAY);
+
+		assertEquals(EmfTestObject.UK_NAME_DAY, configuration.getBusinessKey());
+	}
+}
