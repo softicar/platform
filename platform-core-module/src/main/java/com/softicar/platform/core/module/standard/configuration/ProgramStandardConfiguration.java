6b13434187f8e8c8db67bb88a6c9e97e6576971c
Thees Koester <thees.koester@forspace-solutions.com>
2022-02-28 12:07:16 +0100

    ProgramStandardConfiguration registers default schedule of programs, too

--- platform-core-module/src/main/java/com/softicar/platform/core/module/standard/configuration/ProgramStandardConfiguration.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/standard/configuration/ProgramStandardConfiguration.java
@@ -2,10 +2,15 @@ package com.softicar.platform.core.module.standard.configuration;
 
 import com.softicar.platform.core.module.configuration.AbstractStandardConfiguration;
 import com.softicar.platform.core.module.program.AGProgram;
+import com.softicar.platform.core.module.program.IProgram;
 import com.softicar.platform.core.module.program.Programs;
+import com.softicar.platform.core.module.program.execution.scheduled.AGScheduledProgramExecution;
 import com.softicar.platform.core.module.uuid.AGUuid;
 import com.softicar.platform.core.module.uuid.AGUuidBasedSourceCodeReferencePoint;
 import com.softicar.platform.db.core.transaction.DbTransactions;
+import com.softicar.platform.emf.source.code.reference.point.EmfSourceCodeReferencePoints;
+import com.softicar.platform.emf.source.code.reference.point.IEmfSourceCodeReferencePoint;
+import java.util.Optional;
 
 public class ProgramStandardConfiguration extends AbstractStandardConfiguration {
 
@@ -16,14 +21,29 @@ public class ProgramStandardConfiguration extends AbstractStandardConfiguration 
 			.getAllProgramsAsIndirectEntities()
 			.stream()
 			.map(AGUuidBasedSourceCodeReferencePoint::getRepresentedEntity)
-			.forEach(it -> DbTransactions.wrap(this::registerProgram).accept(it));
+			.forEach(it -> DbTransactions.wrap(this::registerProgramAndDefaultSchedule).accept(it));
 	}
 
-	private void registerProgram(AGUuid uuid) {
+	private void registerProgramAndDefaultSchedule(AGUuid uuid) {
 
 		AGProgram program = new AGProgram()//
 			.setProgramUuid(uuid.getUuid())
 			.save();
 		program.getState().save();
+		registerDefaultSchedule(uuid);
+	}
+
+	private void registerDefaultSchedule(AGUuid uuid) {
+
+		Optional<IEmfSourceCodeReferencePoint> point = EmfSourceCodeReferencePoints.getReferencePoint(uuid.getUuid());
+		String defaultCronExpression = ((IProgram) point.get()).getDefaultCronExpression();
+
+		if (!defaultCronExpression.isEmpty()) {
+			new AGScheduledProgramExecution()//
+				.setActive(true)
+				.setCronExpression(defaultCronExpression)
+				.setProgramUuid(uuid.getUuid())
+				.save();
+		}
 	}
 }
