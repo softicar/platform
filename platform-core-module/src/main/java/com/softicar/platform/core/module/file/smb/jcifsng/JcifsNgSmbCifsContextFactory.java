fee9649a42175e3bf83bd231bee8159f16f8c85a
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-03-04 11:47:24 +0100

    add LMHOSTS

--- platform-core-module/src/main/java/com/softicar/platform/core/module/file/smb/jcifsng/JcifsNgSmbCifsContextFactory.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/file/smb/jcifsng/JcifsNgSmbCifsContextFactory.java
@@ -52,7 +52,7 @@ class JcifsNgSmbCifsContextFactory {
 //			properties.setProperty("jcifs.resolveOrder", "DNS,LMHOSTS,WINS,BCAST");
 
 			// testing
-			properties.setProperty("jcifs.resolveOrder", "DNS");
+			properties.setProperty("jcifs.resolveOrder", "DNS,LMHOSTS");
 			return new BaseContext(new PropertyConfiguration(properties));
 		} catch (CIFSException exception) {
 			throw new SmbIOException(exception);
