66bb277a256d0e1a728842e27bb93936060b6e7d
Daniel Klose <daniel.klose@forspace-solutions.com>
2022-03-07 16:04:47 +0100

    Removed more testcode

--- platform-core-module/src/main/java/com/softicar/platform/core/module/start/page/StartPageDiv.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/start/page/StartPageDiv.java
@@ -1,7 +1,5 @@
 package com.softicar.platform.core.module.start.page;
 
-import com.softicar.platform.core.module.file.stored.AGStoredFile;
-import com.softicar.platform.core.module.file.stored.preview.pdf.StoredFilePdfPreviewPopup;
 import com.softicar.platform.core.module.user.CurrentUser;
 import com.softicar.platform.core.module.user.impersonation.UserImpersonationSessionManager;
 import com.softicar.platform.core.module.user.impersonation.UserImpersonationTerminationDiv;
@@ -9,9 +7,7 @@ import com.softicar.platform.core.module.user.password.change.UserPasswordChange
 import com.softicar.platform.core.module.user.password.policy.SofticarPasswordPolicy;
 import com.softicar.platform.dom.element.DomElementTag;
 import com.softicar.platform.dom.elements.DomDiv;
-import com.softicar.platform.dom.elements.button.popup.DomPopupButton;
 import com.softicar.platform.dom.node.IDomNode;
-import com.softicar.platform.dom.style.CssPixel;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -23,6 +19,7 @@ class StartPageDiv extends DomDiv {
 		createPasswordChangeNode().ifPresent(nodes::add);
 		createUserImpersonationTerminationNode().ifPresent(nodes::add);
 		appendSeparatedByHr(nodes);
+		
 		setMarker(StartPageMarker.MAIN_ELEMENT);
 	}
 
