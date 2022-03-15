deb7748b203e3d59edc8622094977642d7b4a392
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-03-15 21:21:54 +0100

    PLAT-601-7 Now Also EmfTransientAttributeDisplay Does Not Use Dom*Display

--- platform-emf/src/main/java/com/softicar/platform/emf/attribute/transients/EmfTransientAttributeDisplay.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/attribute/transients/EmfTransientAttributeDisplay.java
@@ -1,9 +1,7 @@
 package com.softicar.platform.emf.attribute.transients;
 
+import com.softicar.platform.common.core.number.formatter.BigDecimalFormatter;
 import com.softicar.platform.dom.elements.DomDiv;
-import com.softicar.platform.dom.elements.number.decimal.DomBigDecimalDisplay;
-import com.softicar.platform.dom.elements.number.decimal.DomDoubleDisplay;
-import com.softicar.platform.dom.elements.number.decimal.DomFloatDisplay;
 import java.math.BigDecimal;
 
 public class EmfTransientAttributeDisplay<V> extends DomDiv {
@@ -11,13 +9,12 @@ public class EmfTransientAttributeDisplay<V> extends DomDiv {
 	public EmfTransientAttributeDisplay(V value) {
 
 		if (value != null) {
-			// TODO this needs to be centralized
 			if (value instanceof BigDecimal) {
-				appendChild(new DomBigDecimalDisplay().setValue((BigDecimal) value));
+				appendText(new BigDecimalFormatter().format((BigDecimal) value));
 			} else if (value instanceof Double) {
-				appendChild(new DomDoubleDisplay().setValue((Double) value));
+				appendText(new BigDecimalFormatter().format((Double) value));
 			} else if (value instanceof Float) {
-				appendChild(new DomFloatDisplay().setValue((Float) value));
+				appendText(new BigDecimalFormatter().format((Float) value));
 			} else {
 				appendText(value.toString());
 			}
