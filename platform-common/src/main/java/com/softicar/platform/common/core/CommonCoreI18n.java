a909b2453a9eba2e54874b1feefbfe501d65224c
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-03-17 11:38:34 +0100

    Fixed CommonCoreI18n

--- platform-common/src/main/java/com/softicar/platform/common/core/CommonCoreI18n.java
+++ platform-common/src/main/java/com/softicar/platform/common/core/CommonCoreI18n.java
@@ -13,6 +13,8 @@ public interface CommonCoreI18n {
 		.de("Zifferngruppe zu lang.");
 	I18n0 DIGIT_GROUP_TOO_SHORT = new I18n0("Digit group too short.")//
 		.de("Zifferngruppe zu kurz.");
+	I18n1 ILLEGAL_CHARACTERS_FOR_DECIMAL_NUMBER_ARG1 = new I18n1("Illegal characters for decimal number: %s")//
+		.de("Unzulässige Zeichen für Dezimalzahl: %s");
 	I18n0 THE_DECIMAL_PART_MUST_NOT_CONTAIN_DIGIT_GROUP_SEPARATORS = new I18n0("The decimal part must not contain digit group separators.")//
 		.de("Der Dezimalteil darf keine Zifferngruppentrennzeichen enthalten.");
 	I18n0 THE_DECIMAL_SEPARATOR_MAY_NOT_BE_EMPTY = new I18n0("The decimal separator may not be empty.")//
@@ -30,6 +32,4 @@ public interface CommonCoreI18n {
 				.de("Das Zifferngruppentrennzeichen darf keines der folgenden Zeichen enthalten: %s");
 	I18n0 THE_DIGIT_GROUP_SEPARATOR_MUST_NOT_CONTAIN_DIGITS = new I18n0("The digit group separator must not contain digits.")//
 		.de("Das Zifferngruppentrennzeichen darf keine Ziffern beinhalten.");
-	I18n1 ILLEGAL_CHARACTERS_FOR_DECIMAL_NUMBER_ARG1 = new I18n1("Illegal characters for decimal number: %s")//
-		.de("Unzulässige Zeichen für Dezimalzahl: %s");
 }
