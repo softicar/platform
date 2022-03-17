921ff47473d39f89e5d89065e510a4a0a3b7088a
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-03-17 18:09:31 +0100

    PLAT-752 AGUser: Change localization to non-null

--- platform-core-module/src/main/java/com/softicar/platform/core/module/test/AbstractCoreTest.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/test/AbstractCoreTest.java
@@ -1,5 +1,8 @@
 package com.softicar.platform.core.module.test;
 
+import com.softicar.platform.common.core.i18n.LanguageEnum;
+import com.softicar.platform.core.module.language.AGCoreLanguage;
+import com.softicar.platform.core.module.localization.AGLocalization;
 import com.softicar.platform.core.module.test.fixture.CoreModuleTestFixtureMethods;
 import com.softicar.platform.core.module.user.AGUser;
 import com.softicar.platform.core.module.user.CurrentUser;
@@ -15,7 +18,9 @@ public abstract class AbstractCoreTest extends AbstractDbTest implements IDomTes
 
 	public AbstractCoreTest() {
 
-		CurrentUser.set(insertUserWithoutLogging());
+		AGLocalization localization = insertLocalizationWithoutLogging();
+		AGUser user = insertUserWithoutLogging(localization);
+		CurrentUser.set(user);
 	}
 
 	@Override
@@ -28,14 +33,27 @@ public abstract class AbstractCoreTest extends AbstractDbTest implements IDomTes
 	// ---------------------
 	// We need to insert the user without triggering the logging,
 	// since the logging depends on the current user.
-	private AGUser insertUserWithoutLogging() {
+	private AGUser insertUserWithoutLogging(AGLocalization localization) {
 
 		int userId = AGUser.TABLE//
 			.createInsert()
 			.set(AGUser.LOGIN_NAME, "current.user")
 			.set(AGUser.FIRST_NAME, "Current")
 			.set(AGUser.LAST_NAME, "User")
+			.set(AGUser.LOCALIZATION, localization)
 			.execute();
 		return AGUser.get(userId);
+	}
+
+	private AGLocalization insertLocalizationWithoutLogging() {
+
+		int localizationId = AGLocalization.TABLE//
+			.createInsert()
+			.set(AGLocalization.NAME, "current-user-localization")
+			.set(AGLocalization.LANGUAGE, AGCoreLanguage.getByLanguageEnum(LanguageEnum.ENGLISH).get())
+			.set(AGLocalization.DECIMAL_SEPARATOR, ".")
+			.set(AGLocalization.DIGIT_GROUP_SEPARATOR, "")
+			.execute();
+		return AGLocalization.get(localizationId);
 	}
 }
