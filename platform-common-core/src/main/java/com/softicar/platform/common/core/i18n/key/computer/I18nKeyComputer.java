be19b51f08b980160676f04fd1d26a72de967176
Thees Koester <thees.koester@forspace-solutions.com>
2022-03-17 15:07:49 +0100

    Delete main() method

--- platform-common-core/src/main/java/com/softicar/platform/common/core/i18n/key/computer/I18nKeyComputer.java
+++ platform-common-core/src/main/java/com/softicar/platform/common/core/i18n/key/computer/I18nKeyComputer.java
@@ -104,11 +104,4 @@ public class I18nKeyComputer {
 		}
 		return string.toUpperCase();
 	}
-
-	public static void main(String[] args) {
-
-		String s = "In the next step you can control the replacement of the variables by the entered variable values.";
-		System.out.println(new I18nKeyComputer(s).compute());
-
-	}
 }
