5bc819f765a9d3b1d608d8834660a6f16b2b0664
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-03-22 15:16:56 +0100

    PLAT-744 Disable DbTableConfigurationCodeValidator
    Temporarily disabled `DbTableConfigurationCodeValidator`, until after Platform-17 was released.

--- platform-db-runtime/src/main/java/com/softicar/platform/db/runtime/table/configuration/DbTableConfigurationCodeValidator.java
+++ platform-db-runtime/src/main/java/com/softicar/platform/db/runtime/table/configuration/DbTableConfigurationCodeValidator.java
@@ -4,31 +4,54 @@ import com.softicar.platform.common.code.classpath.iterable.ClasspathFileIterabl
 import com.softicar.platform.common.code.classpath.metadata.ClasspathFilesMetadata;
 import com.softicar.platform.common.core.java.code.validation.JavaCodeValidationEnvironment;
 import com.softicar.platform.common.core.java.code.validator.IJavaCodeValidator;
-import com.softicar.platform.common.core.java.code.validator.JavaCodeValidator;
+import com.softicar.platform.db.core.database.DbDatabaseScope;
+import com.softicar.platform.db.core.test.DbTestDatabase;
 import com.softicar.platform.db.runtime.table.IDbTable;
+import com.softicar.platform.db.runtime.table.creator.DbAutomaticTableCreator;
 import com.softicar.platform.db.runtime.table.finder.DbTableFinder;
 import com.softicar.platform.db.runtime.table.row.IDbTableRow;
+import com.softicar.platform.db.runtime.test.DbRandomAutoIncrementSupplier;
+import java.io.File;
 import java.util.Collection;
+import java.util.function.Predicate;
 
 /**
- * Validates the configuration of every {@link IDbTable}.
+ * Validates the configuration of every {@link IDbTable} in this repository.
+ * <p>
+ * FIXME This is too inefficient. The same tables are checked over and over
+ * again.
  *
  * @author Alexander Schmidt
  */
-@JavaCodeValidator
+//@JavaCodeValidator // FIXME Fix and re-enable this.
 public class DbTableConfigurationCodeValidator implements IJavaCodeValidator {
 
 	@Override
 	public void validate(JavaCodeValidationEnvironment environment) {
 
-		for (IDbTable<?, ?> table: findAllTables(IDbTable.class, IDbTableRow.class)) {
-			table.assertValidConfigurationOrThrow();
+		try (var databaseScope = new DbDatabaseScope(createTestDatabase())) {
+			for (IDbTable<?, ?> table: findAllTables(IDbTable.class, IDbTableRow.class)) {
+				table.assertValidConfigurationOrThrow();
+			}
 		}
+	}
+
+	private DbTestDatabase createTestDatabase() {
+
+		var testDatabase = new DbTestDatabase();
+		testDatabase.addListener(new DbAutomaticTableCreator(new DbRandomAutoIncrementSupplier()));
+		return testDatabase;
 	}
 
 	private <T, R> Collection<T> findAllTables(Class<T> tableClass, Class<R> tableRowClass) {
 
-		ClasspathFilesMetadata metadata = new ClasspathFilesMetadata(new ClasspathFileIterable());
+		ClasspathFilesMetadata metadata = new ClasspathFilesMetadata(new ClasspathFileIterable().setJarFileFilter(isRelevantJarFile()));
 		return new DbTableFinder<>(metadata, tableClass, tableRowClass).findAllTables();
+	}
+
+	private Predicate<File> isRelevantJarFile() {
+
+		// TODO Find a better solution to determine the JAR file that contains IDbTable and IDbTableRow.
+		return jarFile -> jarFile.getName().equals("platform-db-runtime.jar");
 	}
 }
