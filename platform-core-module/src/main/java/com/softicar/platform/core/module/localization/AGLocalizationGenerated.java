94bce55eec37e93b53bc8bc5d60842d89de87298
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-03-31 09:03:30 +0200

    PLAT-761 Made `AGLocalization.digitGroupSeparator` Optional Again (#203)

--- platform-core-module/src/main/java/com/softicar/platform/core/module/localization/AGLocalizationGenerated.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/localization/AGLocalizationGenerated.java
@@ -32,7 +32,7 @@ public class AGLocalizationGenerated extends AbstractDbObject<AGLocalization> {
 	public static final IDbStringField<AGLocalization> NAME = BUILDER.addStringField("name", o->o.m_name, (o,v)->o.m_name=v).setTitle(CoreI18n.NAME).setMaximumLength(255);
 	public static final IDbForeignField<AGLocalization, AGCoreLanguage> LANGUAGE = BUILDER.addForeignField("language", o->o.m_language, (o,v)->o.m_language=v, AGCoreLanguage.ID).setTitle(CoreI18n.LANGUAGE);
 	public static final IDbStringField<AGLocalization> DECIMAL_SEPARATOR = BUILDER.addStringField("decimalSeparator", o->o.m_decimalSeparator, (o,v)->o.m_decimalSeparator=v).setTitle(CoreI18n.DECIMAL_SEPARATOR).setMaximumLength(255);
-	public static final IDbStringField<AGLocalization> DIGIT_GROUP_SEPARATOR = BUILDER.addStringField("digitGroupSeparator", o->o.m_digitGroupSeparator, (o,v)->o.m_digitGroupSeparator=v).setTitle(CoreI18n.DIGIT_GROUP_SEPARATOR).setMaximumLength(255);
+	public static final IDbStringField<AGLocalization> DIGIT_GROUP_SEPARATOR = BUILDER.addStringField("digitGroupSeparator", o->o.m_digitGroupSeparator, (o,v)->o.m_digitGroupSeparator=v).setTitle(CoreI18n.DIGIT_GROUP_SEPARATOR).setDefault("").setMaximumLength(255);
 	public static final IDbKey<AGLocalization> UK_NAME = BUILDER.addUniqueKey("name", NAME);
 	public static final AGLocalizationTable TABLE = new AGLocalizationTable(BUILDER);
 	// @formatter:on
