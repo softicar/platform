9fc09a8d0799c6344385162b492acbc8b18c17e8
Thees Koester <thees.koester@forspace-solutions.com>
2022-04-13 09:57:59 +0200

    Added EmfTestModuleInstance.sqml and related classes

---
+++ platform-emf/src/test/java/com/softicar/platform/emf/management/importing/engine/EmfImportColumnsCollectorTest.java
@@ -0,0 +1,38 @@
+package com.softicar.platform.emf.management.importing.engine;
+
+import com.softicar.platform.db.runtime.test.AbstractDbTest;
+import org.junit.Test;
+
+public class EmfImportColumnsCollectorTest extends AbstractDbTest {
+
+	@Test
+	public void testGetCsvFileColumnsToImport() {
+
+		// TODO implement
+	}
+
+	@Test
+	public void testGetTableColumns() {
+
+		// TODO implement
+	}
+
+	@Test
+	public void testGetFieldsOfTableColumns() {
+
+		// TODO implement
+	}
+
+	@Test
+	public void testGetFieldOfTableColumnByIndex() {
+
+		// TODO implement
+	}
+
+	@Test
+	public void testGetTable() {
+
+		// TODO implement
+	}
+
+}
