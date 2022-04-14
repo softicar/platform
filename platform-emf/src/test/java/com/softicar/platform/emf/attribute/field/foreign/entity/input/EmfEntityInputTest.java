97b681e40d168a195ca91d7c4959803724533464
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-04-14 10:13:58 +0200

    PLAT-763 Auto-Complete inputs: Do not infer entries from ID substrings (#215)

--- platform-emf/src/test/java/com/softicar/platform/emf/attribute/field/foreign/entity/input/EmfEntityInputTest.java
+++ platform-emf/src/test/java/com/softicar/platform/emf/attribute/field/foreign/entity/input/EmfEntityInputTest.java
@@ -2,38 +2,80 @@ package com.softicar.platform.emf.attribute.field.foreign.entity.input;
 
 import com.softicar.platform.dom.input.DomTextInput;
 import com.softicar.platform.emf.AbstractEmfTest;
-import com.softicar.platform.emf.test.user.EmfTestUser;
+import com.softicar.platform.emf.EmfI18n;
+import com.softicar.platform.emf.test.simple.EmfTestObject;
 import org.junit.Test;
 
 public class EmfEntityInputTest extends AbstractEmfTest {
 
+	private final EmfTestObject object1;
+	private final EmfTestObject object23;
+
 	public EmfEntityInputTest() {
 
-		setNodeSupplier(() -> new EmfEntityInput<>(new EmfEntityInputEngine<>(EmfTestUser.TABLE)));
+		setNodeSupplier(() -> new EmfEntityInput<>(new EmfEntityInputEngine<>(EmfTestObject.TABLE)));
+		this.object1 = insertTestObject(1, "one");
+		this.object23 = insertTestObject(23, "twentythree");
 	}
 
 	@Test
-	public void testGetValueWithIntegerInput() {
+	public void testGetValueWithObject1AndId() {
 
-		setInputValue(user.getId() + "");
-		assertSame(user, getInputValue());
+		setInputValue("1");
+		assertSame(object1, getValue());
 	}
 
 	@Test
-	public void testGetValueWithDisplayNameWithId() {
+	public void testGetValueWithObject1AndName() {
 
-		setInputValue(user.toDisplay().toString());
-		assertSame(user, getInputValue());
+		setInputValue("one");
+		assertSame(object1, getValue());
 	}
 
 	@Test
-	public void testGetValueWithFirstName() {
+	public void testGetValueWithObject1AndNameAndId() {
 
-		setInputValue(user.getFirstName());
-		assertSame(user, getInputValue());
+		setInputValue("one [1]");
+		assertSame(object1, getValue());
 	}
 
-	private Object getInputValue() {
+	@Test
+	public void testGetValueWithObject1AndNameAndIdIncomplete() {
+
+		setInputValue("one [1");
+		assertException(this::getValue, EmfI18n.PLEASE_SELECT_A_VALID_ENTRY);
+	}
+
+	@Test
+	public void testGetValueWithObject23AndId() {
+
+		setInputValue("23");
+		assertSame(object23, getValue());
+	}
+
+	/**
+	 * Assert that we do <b>not</b> infer ID "23" from the head (leading
+	 * substring) "2" of that ID.
+	 */
+	@Test
+	public void testGetValueWithObject23AndIdHead() {
+
+		setInputValue("2");
+		assertException(this::getValue, EmfI18n.PLEASE_SELECT_A_VALID_ENTRY);
+	}
+
+	/**
+	 * Assert that we do <b>not</b> infer ID "23" from the tail (tailing
+	 * substring) "3" of that ID.
+	 */
+	@Test
+	public void testGetValueWithObject23AndIdTail() {
+
+		setInputValue("3");
+		assertException(this::getValue, EmfI18n.PLEASE_SELECT_A_VALID_ENTRY);
+	}
+
+	private Object getValue() {
 
 		return findBody()//
 			.findNode(EmfEntityInput.class)
@@ -47,5 +89,15 @@ public class EmfEntityInputTest extends AbstractEmfTest {
 		findBody()//
 			.findNode(DomTextInput.class)
 			.setInputValue(value);
+	}
+
+	private EmfTestObject insertTestObject(int id, String name) {
+
+		int insertedId = EmfTestObject.TABLE//
+			.createInsert()
+			.set(EmfTestObject.ID, id)
+			.set(EmfTestObject.NAME, name)
+			.execute();
+		return EmfTestObject.TABLE.load(insertedId);
 	}
 }
