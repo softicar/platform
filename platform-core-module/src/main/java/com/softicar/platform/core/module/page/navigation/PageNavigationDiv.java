c0345b398c93ad9a85ecfe5f51ab792bec6c4849
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-04-20 22:30:58 +0200

    PLAT-799 Let PageNavigationDiv implement IDomRefreshBusListener (#220)

--- platform-core-module/src/main/java/com/softicar/platform/core/module/page/navigation/PageNavigationDiv.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/page/navigation/PageNavigationDiv.java
@@ -4,8 +4,10 @@ import com.softicar.platform.core.module.page.PageHeaderAndContentDiv;
 import com.softicar.platform.core.module.page.navigation.link.PageNavigationLinkLoader;
 import com.softicar.platform.core.module.page.navigation.link.display.PageNavigationFolderContentDiv;
 import com.softicar.platform.dom.elements.DomDiv;
+import com.softicar.platform.dom.refresh.bus.IDomRefreshBusEvent;
+import com.softicar.platform.dom.refresh.bus.IDomRefreshBusListener;
 
-public class PageNavigationDiv extends DomDiv {
+public class PageNavigationDiv extends DomDiv implements IDomRefreshBusListener {
 
 	private final PageNavigationFolderController folderController;
 	private final PageNavigationPageController pageController;
@@ -17,8 +19,15 @@ public class PageNavigationDiv extends DomDiv {
 
 		setCssClass(PageNavigationCssClasses.PAGE_NAVIGATION_DIV);
 
-		appendChild(new PageNavigationLogoDiv());
-		appendChild(new PageNavigationFolderContentDiv(this, null, PageNavigationLinkLoader.loadRootLink()));
+		refresh();
+	}
+
+	@Override
+	public void refresh(IDomRefreshBusEvent event) {
+
+		if (event.isAnyObjectChanged(PageNavigationDiv.class)) {
+			refresh();
+		}
 	}
 
 	public PageNavigationFolderController getFolderController() {
@@ -29,5 +38,12 @@ public class PageNavigationDiv extends DomDiv {
 	public PageNavigationPageController getPageController() {
 
 		return pageController;
+	}
+
+	private void refresh() {
+
+		removeChildren();
+		appendChild(new PageNavigationLogoDiv());
+		appendChild(new PageNavigationFolderContentDiv(this, null, PageNavigationLinkLoader.loadRootLink()));
 	}
 }
