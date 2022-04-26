668aab90c8b9541e8685d22a0038c646c86b83d5
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-04-26 17:17:50 +0200

    PLAT-728-2 Removed Unnecessary Use of AbstractEmfInputDiv

--- platform-emf/src/main/java/com/softicar/platform/emf/attribute/field/item/EmfReadOnlyBasicEntityInput.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/attribute/field/item/EmfReadOnlyBasicEntityInput.java
@@ -1,10 +1,11 @@
 package com.softicar.platform.emf.attribute.field.item;
 
 import com.softicar.platform.common.core.entity.IEntity;
-import com.softicar.platform.emf.attribute.input.AbstractEmfInputDiv;
+import com.softicar.platform.dom.elements.DomDiv;
+import com.softicar.platform.emf.attribute.input.IEmfInput;
 import java.util.Optional;
 
-public class EmfReadOnlyBasicEntityInput<V extends IEntity> extends AbstractEmfInputDiv<V> {
+public class EmfReadOnlyBasicEntityInput<V extends IEntity> extends DomDiv implements IEmfInput<V> {
 
 	private V value;
 
