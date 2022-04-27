02c87aa8ebb20333519f40c3e6b157bad7f25d81
Thees Koester <thees.koester@forspace-solutions.com>
2022-04-27 15:24:14 +0200

    Temporary changes in AGDemoInvoice

--- platform-demo-module/src/main/java/com/softicar/platform/demo/module/invoice/item/AGDemoInvoiceItem.java
+++ platform-demo-module/src/main/java/com/softicar/platform/demo/module/invoice/item/AGDemoInvoiceItem.java
@@ -1,6 +1,7 @@
 package com.softicar.platform.demo.module.invoice.item;
 
 import com.softicar.platform.common.core.i18n.IDisplayString;
+import com.softicar.platform.emf.management.importing.engine.EmfImportColumnsStructure;
 import com.softicar.platform.emf.object.IEmfObject;
 
 public class AGDemoInvoiceItem extends AGDemoInvoiceItemGenerated implements IEmfObject<AGDemoInvoiceItem> {
@@ -9,5 +10,10 @@ public class AGDemoInvoiceItem extends AGDemoInvoiceItemGenerated implements IEm
 	public IDisplayString toDisplayWithoutId() {
 
 		return IDisplayString.create(getItem());
+	}
+
+	public static void main(String[] args) {
+
+		new EmfImportColumnsStructure<>(AGDemoInvoiceItem.TABLE).getTableColumns();
 	}
 }
