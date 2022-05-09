372b4064d52a498af11da45a2273a15a5611bc9d
Thees Koester <thees.koester@forspace-solutions.com>
2022-05-09 11:07:52 +0200

    Set BK of AGDemoInvoiceItem

--- platform-demo-module/src/main/java/com/softicar/platform/demo/module/invoice/item/AGDemoInvoiceItemTable.java
+++ platform-demo-module/src/main/java/com/softicar/platform/demo/module/invoice/item/AGDemoInvoiceItemTable.java
@@ -19,5 +19,6 @@ public class AGDemoInvoiceItemTable extends EmfObjectTable<AGDemoInvoiceItem, AG
 		configuration.setScopeField(AGDemoInvoiceItem.INVOICE);
 		configuration.setCreationPredicate(DemoInvoicePredicates.NOT_LOCKED_ITEMS);
 		configuration.setEditPredicate(DemoInvoiceItemPredicates.NOT_LOCKED_ITEMS);
+		configuration.setBusinessKey(AGDemoInvoiceItem.UK_INVOICE_ITEM);
 	}
 }
