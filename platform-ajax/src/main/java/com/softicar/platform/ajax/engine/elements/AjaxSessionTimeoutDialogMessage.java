3b58254bb5f6b60cc4483c7c1808908d05b55a34
Daniel Klose <daniel.klose@forspace-solutions.com>
2022-05-16 09:44:43 +0200

    PLAT-855 Adapt SessionTimeoutDialogMessage

--- platform-ajax/src/main/java/com/softicar/platform/ajax/engine/elements/AjaxSessionTimeoutDialogMessage.java
+++ platform-ajax/src/main/java/com/softicar/platform/ajax/engine/elements/AjaxSessionTimeoutDialogMessage.java
@@ -2,7 +2,10 @@ package com.softicar.platform.ajax.engine.elements;
 
 import com.softicar.platform.ajax.AjaxCssClasses;
 import com.softicar.platform.ajax.AjaxI18n;
+import com.softicar.platform.common.core.CommonCoreI18n;
 import com.softicar.platform.dom.elements.DomDiv;
+import com.softicar.platform.dom.elements.bar.DomActionBar;
+import com.softicar.platform.dom.elements.button.DomButton;
 
 public class AjaxSessionTimeoutDialogMessage extends DomDiv {
 
@@ -10,6 +13,8 @@ public class AjaxSessionTimeoutDialogMessage extends DomDiv {
 
 		addCssClass(AjaxCssClasses.AJAX_SESSION_TIMEOUT_DIALOG_MESSAGE);
 		appendText(AjaxI18n.YOUR_SESSION_TIMED_OUT);
-		getDomEngine().setReloadPageOnClick(this);
+		DomButton button = new DomButton().setLabel(CommonCoreI18n.RETURN_TO_LOGIN);
+		appendChild(new DomActionBar(button));
+		getDomEngine().setReloadPageOnClick(button);
 	}
 }
