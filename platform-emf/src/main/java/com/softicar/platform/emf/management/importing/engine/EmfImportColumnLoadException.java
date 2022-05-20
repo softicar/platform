0126cf0b5e0b6b04ad3c2bf8ef74a9fdf47c0bef
Thees Koester <thees.koester@forspace-solutions.com>
2022-05-20 14:55:34 +0200

    Javadoc for EmfImportColumnLoadException

--- platform-emf/src/main/java/com/softicar/platform/emf/management/importing/engine/EmfImportColumnLoadException.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/management/importing/engine/EmfImportColumnLoadException.java
@@ -4,11 +4,21 @@ public class EmfImportColumnLoadException extends RuntimeException {
 
 	private final EmfImportColumn<?, ?> column;
 
+	/**
+	 * @param column
+	 *            the column that actually should store the value, but that
+	 *            could not be loaded
+	 */
 	public EmfImportColumnLoadException(EmfImportColumn<?, ?> column) {
 
 		this.column = column;
 	}
 
+	/**
+	 * @return The column that actually should store the value, but that could
+	 *         not be loaded what triggered this
+	 *         {@link EmfImportColumnLoadException}
+	 */
 	public EmfImportColumn<?, ?> getColumn() {
 
 		return column;
