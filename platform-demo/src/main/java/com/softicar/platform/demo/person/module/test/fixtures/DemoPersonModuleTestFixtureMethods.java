ff46d0cd26ce6d9ea780a2f1538b2919a99f54e9
Thees Koester <thees.koester@forspace-solutions.com>
2022-05-20 14:55:06 +0200

    DemoPersonModuleTestFixtureMethods extends DemoInvoiceModuleTestFixtureMethods now

--- platform-demo/src/main/java/com/softicar/platform/demo/person/module/test/fixtures/DemoPersonModuleTestFixtureMethods.java
+++ platform-demo/src/main/java/com/softicar/platform/demo/person/module/test/fixtures/DemoPersonModuleTestFixtureMethods.java
@@ -1,12 +1,12 @@
 package com.softicar.platform.demo.person.module.test.fixtures;
 
 import com.softicar.platform.common.date.Day;
-import com.softicar.platform.core.module.test.fixture.CoreModuleTestFixtureMethods;
 import com.softicar.platform.demo.core.module.AGDemoCoreModuleInstance;
+import com.softicar.platform.demo.invoice.module.test.fixture.DemoInvoiceModuleTestFixtureMethods;
 import com.softicar.platform.demo.person.module.AGDemoPerson;
 import com.softicar.platform.demo.person.module.AGDemoPersonModuleInstance;
 
-public interface DemoPersonModuleTestFixtureMethods extends CoreModuleTestFixtureMethods {
+public interface DemoPersonModuleTestFixtureMethods extends DemoInvoiceModuleTestFixtureMethods {
 
 	default AGDemoPersonModuleInstance insertDemoPersonModuleInstance(AGDemoCoreModuleInstance demoCoreModuleInstance) {
 
