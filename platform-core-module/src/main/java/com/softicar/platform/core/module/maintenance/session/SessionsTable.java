9e071f34058b36b25da951c6765fe0a29f8fae21
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-05-20 13:32:33 +0200

    PLAT-879 Sessions Page: Improve user column
    - Use FK popup buttons.
    - Show a placeholder when the user is unknown.

--- platform-core-module/src/main/java/com/softicar/platform/core/module/maintenance/session/SessionsTable.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/maintenance/session/SessionsTable.java
@@ -1,5 +1,6 @@
 package com.softicar.platform.core.module.maintenance.session;
 
+import com.softicar.platform.common.container.data.table.IDataTableColumn;
 import com.softicar.platform.common.container.data.table.in.memory.AbstractInMemoryDataTable;
 import com.softicar.platform.common.date.DayTime;
 import com.softicar.platform.common.date.Duration;
@@ -11,9 +12,11 @@ import javax.servlet.http.HttpSession;
 
 public class SessionsTable extends AbstractInMemoryDataTable<HttpSession> {
 
+	private final IDataTableColumn<HttpSession, AGUser> userColumn;
+
 	public SessionsTable() {
 
-		newColumn(AGUser.class)//
+		this.userColumn = newColumn(AGUser.class)//
 			.setGetter(this::getUserFromSession)
 			.setTitle(CoreI18n.USER)
 			.addColumn();
@@ -37,6 +40,11 @@ public class SessionsTable extends AbstractInMemoryDataTable<HttpSession> {
 			.setGetter(session -> getElapsedTime(session.getLastAccessedTime()))
 			.setTitle(CoreI18n.INACTIVE_FOR)
 			.addColumn();
+	}
+
+	public IDataTableColumn<HttpSession, AGUser> getUserColumn() {
+
+		return userColumn;
 	}
 
 	@Override
