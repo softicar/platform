1e7ab303b9cf46e35b4fec82e015dc39f02c9da8
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-06-15 19:29:49 +0200

    PLAT-917 Rename IDomPopupMaximizationContext to IDomPopupContext (#318)

--- platform-dom/src/main/java/com/softicar/platform/dom/elements/popup/compositor/IDomPopupContext.java
+++ platform-dom/src/main/java/com/softicar/platform/dom/elements/popup/compositor/IDomPopupContext.java
@@ -5,15 +5,15 @@ import com.softicar.platform.dom.elements.popup.DomPopupFrame;
 import com.softicar.platform.dom.parent.IDomParentElement;
 
 /**
- * An {@link IDomParentElement} that serves as a container for a maximized
- * {@link DomPopup}.
+ * An {@link IDomParentElement} that serves as a parent for the
+ * {@link DomPopupFrame} of a {@link DomPopup}.
  * <p>
- * The {@link DomPopupFrame} of any maximized {@link DomPopup} is appended to an
- * element that implements this marker interface.
+ * The {@link DomPopupFrame} of any {@link DomPopup} is appended to an element
+ * that implements this marker interface.
  *
  * @author Alexander Schmidt
  */
-public interface IDomPopupMaximizationContext extends IDomParentElement {
+public interface IDomPopupContext extends IDomParentElement {
 
 	// nothing
 }
