8eac9eae35b2faf2dcb43284dc304f56e3af74cd
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-06-20 16:45:24 +0200

    added old method back to SofticarTestDatabase as deprecated

--- platform-core-module/src/main/java/com/softicar/platform/core/module/test/SofticarTestDatabase.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/test/SofticarTestDatabase.java
@@ -1,7 +1,9 @@
 package com.softicar.platform.core.module.test;
 
+import com.softicar.platform.db.core.database.IDbDatabase;
 import com.softicar.platform.db.core.test.DbTestDatabase;
 import com.softicar.platform.db.runtime.table.creator.DbAutomaticTableCreator;
+import java.util.function.Supplier;
 
 /**
  * Extension of the {@link DbTestDatabase}.
@@ -15,5 +17,14 @@ public class SofticarTestDatabase extends DbTestDatabase {
 	public SofticarTestDatabase() {
 
 		addListener(new DbAutomaticTableCreator(() -> 1));
+	}
+
+	/**
+	 * @deprecated use {@link IDbDatabase#apply}
+	 */
+	@Deprecated
+	public <T> T applyFixture(Supplier<T> supplier) {
+
+		return super.apply(supplier);
 	}
 }
