47c1fe64676bca4953ad707bd81fb779d1bb9e1c
Daniel Klose <daniel.klose@forspace-solutions.com>
2022-07-26 11:32:07 +0200

    added WIP view permission check

--- platform-emf/src/main/java/com/softicar/platform/emf/form/EmfFormAttributesDiv.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/form/EmfFormAttributesDiv.java
@@ -1,5 +1,7 @@
 package com.softicar.platform.emf.form;
 
+import com.softicar.platform.common.core.user.CurrentBasicUser;
+import com.softicar.platform.common.core.utils.CastUtils;
 import com.softicar.platform.emf.attribute.IEmfAttribute;
 import com.softicar.platform.emf.editor.EmfAttributesDiv;
 import com.softicar.platform.emf.table.row.IEmfTableRow;
@@ -11,7 +13,7 @@ public class EmfFormAttributesDiv<R extends IEmfTableRow<R, ?>> extends EmfAttri
 		super(tableRow, editMode);
 
 		for (IEmfAttribute<R, ?> attribute: tableRow.table().getAttributes()) {
-			if (attribute.isConcealed()) {
+			if (attribute.isConcealed() || isScopeViewPermissionMissing(attribute)) {
 				// skip concealed attributes
 			} else if (tableRow.impermanent() && attribute.isTransient()) {
 				// skip transient attributes in creation mode
@@ -19,5 +21,23 @@ public class EmfFormAttributesDiv<R extends IEmfTableRow<R, ?>> extends EmfAttri
 				addAttribute(attribute);
 			}
 		}
+	}
+
+	//FIXME This works, but needs simplification
+	private boolean isScopeViewPermissionMissing(IEmfAttribute<R, ?> attribute) {
+
+		var scopeAttribute = tableRow.table().getScopeAttribute();
+		if (scopeAttribute.map(it -> it.equals(attribute)).orElse(false)) {
+			var scope = scopeAttribute.get().getValue(tableRow);
+			return !scopeAttribute//
+				.get()
+				.asForeignRowAttribute()
+				.get()
+				.getTargetTable()
+				.getAuthorizer()
+				.getViewPermission()
+				.test(CastUtils.cast(scope), CurrentBasicUser.get());
+		}
+		return false;
 	}
 }
