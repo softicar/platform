f365aea2522d5493b08168b0c6a8f91b927cbe64
Daniel Klose <daniel.klose@forspace-solutions.com>
2022-07-26 11:17:29 +0200

    PLAT-1043 Show Scope in EmfForm

--- platform-emf/src/main/java/com/softicar/platform/emf/attribute/field/EmfFieldAttribute.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/attribute/field/EmfFieldAttribute.java
@@ -110,7 +110,7 @@ public class EmfFieldAttribute<R extends IEmfTableRow<R, ?>, V> extends Abstract
 	@Override
 	public boolean isConcealed() {
 
-		return concealed || isImplicitlyConcealed();
+		return concealed;
 	}
 
 	@Override
@@ -216,19 +216,5 @@ public class EmfFieldAttribute<R extends IEmfTableRow<R, ?>, V> extends Abstract
 
 		this.immutable = immutable;
 		return this;
-	}
-
-	private boolean isImplicitlyConcealed() {
-
-		return isScopeAttribute(getTable());
-	}
-
-	private <S> boolean isScopeAttribute(IEmfTable<R, ?, S> table) {
-
-		return table//
-			.getScopeField()
-			.map(getTable()::getAttribute)
-			.filter(this::equals)
-			.isPresent();
 	}
 }
--- platform-emf/src/main/java/com/softicar/platform/emf/management/EmfManagementDataTable.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/management/EmfManagementDataTable.java
@@ -141,7 +141,7 @@ public class EmfManagementDataTable<R extends IEmfTableRow<R, P>, P, S> extends 
 
 	private <V> boolean showAttribute(IEmfAttribute<R, V> attribute) {
 
-		if (attribute.isConcealed()) {
+		if (attribute.isConcealed() || entityTable.getScopeAttribute().map(it -> it.equals(attribute)).orElse(false)) {
 			return false;
 		} else if (!showInactive && isActiveAttribute(attribute)) {
 			return false;
