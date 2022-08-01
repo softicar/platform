b3e2f04ab489a7289c7bb28cdf2cd6330dd4f20e
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-08-01 15:55:22 +0200

    PLAT-1060 EmfTransientAttributeDisplay Now Respects Date Format (#410)

--- platform-emf/src/main/java/com/softicar/platform/emf/attribute/transients/EmfTransientAttributeDisplay.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/attribute/transients/EmfTransientAttributeDisplay.java
@@ -1,5 +1,6 @@
 package com.softicar.platform.emf.attribute.transients;
 
+import com.softicar.platform.common.core.i18n.IDisplayable;
 import com.softicar.platform.common.core.number.formatter.BigDecimalFormatter;
 import com.softicar.platform.dom.elements.DomDiv;
 import java.math.BigDecimal;
@@ -15,6 +16,8 @@ public class EmfTransientAttributeDisplay<V> extends DomDiv {
 				appendText(new BigDecimalFormatter().format((Double) value));
 			} else if (value instanceof Float) {
 				appendText(new BigDecimalFormatter().format((Float) value));
+			} else if (value instanceof IDisplayable) {
+				appendText(((IDisplayable) value).toDisplay());
 			} else {
 				appendText(value.toString());
 			}
