76dd6df8edbf774cab3dbc6474d80cc4bf0302e8
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-08-01 14:48:00 +0200

    generalized file type matching of StoredFileViewOrDownloadButton

--- platform-core-module/src/main/java/com/softicar/platform/core/module/file/stored/download/StoredFileViewOrDownloadButton.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/file/stored/download/StoredFileViewOrDownloadButton.java
@@ -4,10 +4,7 @@ import com.softicar.platform.common.core.i18n.IDisplayString;
 import com.softicar.platform.common.core.interfaces.INullaryVoidFunction;
 import com.softicar.platform.common.core.logging.Log;
 import com.softicar.platform.common.io.mime.IMimeType;
-import com.softicar.platform.common.io.mime.MimeType;
-import com.softicar.platform.common.io.resource.IResource;
 import com.softicar.platform.common.string.formatting.StackTraceFormatting;
-import com.softicar.platform.core.module.CoreImages;
 import com.softicar.platform.core.module.file.stored.AGStoredFile;
 import com.softicar.platform.core.module.file.stored.StoredFileResource;
 import com.softicar.platform.core.module.file.stored.preview.image.StoredFileImagePreviewPopup;
@@ -18,11 +15,7 @@ import com.softicar.platform.dom.elements.button.DomButton;
 import com.softicar.platform.dom.elements.popup.DomPopup;
 import com.softicar.platform.dom.style.CssPixel;
 import com.softicar.platform.dom.style.ICssLength;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.TreeMap;
 
 public class StoredFileViewOrDownloadButton extends DomButton {
 
@@ -31,7 +24,7 @@ public class StoredFileViewOrDownloadButton extends DomButton {
 	private final AGStoredFile file;
 	private final StoredFileResource resource;
 	private IDisplayString buttonLabel;
-	private final FileType fileType;
+	private final StoredFileViewType fileType;
 	private INullaryVoidFunction preClickCallback;
 
 	private PopupPosition popupPosition = PopupPosition.CENTERED_ON_VIEWPORT;
@@ -87,9 +80,9 @@ public class StoredFileViewOrDownloadButton extends DomButton {
 		return this;
 	}
 
-	private FileType getFileType(StoredFileResource resource) {
+	private StoredFileViewType getFileType(StoredFileResource resource) {
 
-		return FileType.getByIdentifier(getMimeTypeIdentifier(resource));
+		return StoredFileViewType.getByIdentifier(getMimeTypeIdentifier(resource));
 	}
 
 	private String getMimeTypeIdentifier(StoredFileResource resource) {
@@ -153,44 +146,5 @@ public class StoredFileViewOrDownloadButton extends DomButton {
 	private void downloadFile() {
 
 		CurrentDomDocument.get().getBody().appendChild(new StoredFileDownloadIFrame(file));
-	}
-
-	private static enum FileType {
-
-		IMAGE(CoreImages.FILE_TYPE_IMAGE.getResource(), MimeType.IMAGE_JPEG, MimeType.IMAGE_PNG, MimeType.IMAGE_GIF, MimeType.IMAGE_BMP,
-				MimeType.IMAGE_SVG_XML),
-		PDF(CoreImages.FILE_TYPE_PDF.getResource(), MimeType.APPLICATION_PDF),
-		TEXT(CoreImages.FILE_TYPE_TEXT.getResource(), MimeType.TEXT_CSS, MimeType.TEXT_CSV, MimeType.TEXT_HTML, MimeType.TEXT_JAVASCRIPT, MimeType.TEXT_PLAIN,
-				MimeType.TEXT_XML),
-		UNKNOWN(CoreImages.FILE_TYPE_UNKNOWN.getResource());
-
-		private List<IMimeType> mimeTypes;
-		private final IResource icon;
-
-		private FileType(IResource icon, IMimeType...mimeTypes) {
-
-			this.icon = icon;
-			this.mimeTypes = Arrays.asList(mimeTypes);
-		}
-
-		public IResource getIcon() {
-
-			return icon;
-		}
-
-		private static final Map<String, FileType> MAP = new TreeMap<>();
-
-		static {
-			for (FileType fileType: FileType.values()) {
-				fileType.mimeTypes.stream().forEach(mimeType -> MAP.put(mimeType.getIdentifier(), fileType));
-			}
-		}
-
-		public static FileType getByIdentifier(String mimeTypeIdentifier) {
-
-			return Optional//
-				.ofNullable(MAP.get(mimeTypeIdentifier))
-				.orElse(UNKNOWN);
-		}
 	}
 }
---
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/file/stored/download/StoredFileViewType.java
@@ -0,0 +1,41 @@
+package com.softicar.platform.core.module.file.stored.download;
+
+import com.softicar.platform.common.io.resource.IResource;
+import com.softicar.platform.core.module.CoreImages;
+import java.util.regex.Pattern;
+
+enum StoredFileViewType {
+
+	// IMPORTANT: keep order from most specific to least specific
+	PDF(CoreImages.FILE_TYPE_PDF.getResource(), "application/pdf"),
+	IMAGE(CoreImages.FILE_TYPE_IMAGE.getResource(), "image/.*"),
+	TEXT(CoreImages.FILE_TYPE_TEXT.getResource(), "text/.*"),
+	UNKNOWN(CoreImages.FILE_TYPE_UNKNOWN.getResource(), ".*");
+
+	private final IResource icon;
+	private Pattern pattern;
+
+	private StoredFileViewType(IResource icon, String regex) {
+
+		this.icon = icon;
+		this.pattern = Pattern.compile(regex);
+	}
+
+	public IResource getIcon() {
+
+		return icon;
+	}
+
+	public static StoredFileViewType getByIdentifier(String mimeTypeIdentifier) {
+
+		mimeTypeIdentifier = mimeTypeIdentifier.toLowerCase();
+
+		for (StoredFileViewType type: values()) {
+			if (type.pattern.matcher(mimeTypeIdentifier).matches()) {
+				return type;
+			}
+		}
+
+		return UNKNOWN;
+	}
+}
