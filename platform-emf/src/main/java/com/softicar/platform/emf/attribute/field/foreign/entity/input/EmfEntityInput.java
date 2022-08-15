28c4613c7373ce240a599c69c914de38030b9b50
Daniel Klose <daniel.klose@forspace-solutions.com>
2022-08-15 15:40:57 +0200

    removed interface

--- platform-emf/src/main/java/com/softicar/platform/emf/attribute/field/foreign/entity/input/EmfEntityInput.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/attribute/field/foreign/entity/input/EmfEntityInput.java
@@ -1,28 +1,35 @@
 package com.softicar.platform.emf.attribute.field.foreign.entity.input;
 
+import com.softicar.platform.common.core.interfaces.INullaryVoidFunction;
 import com.softicar.platform.dom.elements.input.auto.DomAutoCompleteInput;
 import com.softicar.platform.dom.elements.input.auto.IDomAutoCompleteInputEngine;
-import com.softicar.platform.emf.attribute.field.foreign.entity.IEmfEntityInput;
 import com.softicar.platform.emf.attribute.field.foreign.entity.input.edit.EmfEntityInputEditButton;
+import com.softicar.platform.emf.attribute.input.IEmfInput;
 import com.softicar.platform.emf.attribute.input.auto.complete.EmfAutoCompleteBrowseButton;
 import com.softicar.platform.emf.entity.IEmfEntity;
 import com.softicar.platform.emf.entity.table.IEmfEntityTable;
 
-public class EmfEntityInput<E extends IEmfEntity<E, ?>> extends DomAutoCompleteInput<E> implements IEmfEntityInput<E> {
+public class EmfEntityInput<E extends IEmfEntity<E, ?>> extends DomAutoCompleteInput<E> implements IEmfInput<E> {
 
 	private final EmfEntityInputEditButton<E> editButton;
+	private INullaryVoidFunction refreshCallback;
 
 	public EmfEntityInput(IDomAutoCompleteInputEngine<E> inputEngine) {
 
 		super(inputEngine);
-
 		inputBar.appendChild(new EmfAutoCompleteBrowseButton(this, inputEngine));
 		inputBar.appendChild(this.editButton = new EmfEntityInputEditButton<>(this));
+		this.refreshCallback = INullaryVoidFunction.NO_OPERATION;
 	}
 
 	public <S> EmfEntityInput(IEmfEntityTable<E, ?, S> targetTable, S scope) {
 
 		this(new EmfEntityInputEngine<>(targetTable, scope));
+	}
+
+	public void setRefreshCallback(INullaryVoidFunction refreshCallback) {
+
+		this.refreshCallback = refreshCallback;
 	}
 
 	@Override
@@ -43,6 +50,6 @@ public class EmfEntityInput<E extends IEmfEntity<E, ?>> extends DomAutoCompleteI
 
 		super.refreshInputConstraints();
 		editButton.refresh(getValueNoThrow());
-		refreshInput();
+		refreshCallback.apply();
 	}
 }
