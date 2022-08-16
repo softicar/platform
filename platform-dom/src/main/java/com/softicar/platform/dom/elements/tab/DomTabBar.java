b33d98a94a1482b382ecfc8f189873edff6a6e2a
Daniel Klose <daniel.klose@forspace-solutions.com>
2022-08-16 13:51:46 +0200

    PLAT-1059 Adapt EmfFormPopupTester.clickTab (#437)

--- platform-dom/src/main/java/com/softicar/platform/dom/elements/tab/DomTabBar.java
+++ platform-dom/src/main/java/com/softicar/platform/dom/elements/tab/DomTabBar.java
@@ -10,20 +10,20 @@ import java.util.List;
 public class DomTabBar extends DomDiv {
 
 	private final List<DomTab> tabs;
-	private final TabBarHeader tabBarHeaderContainer;
+	private final DomTabBarHeader tabBarHeader;
 	private final DomTabBarContentContainer tabContentContainer;
 	private DomTab currentTab;
 
 	public DomTabBar() {
 
 		this.tabs = new ArrayList<>();
-		this.tabBarHeaderContainer = new TabBarHeader();
+		this.tabBarHeader = new DomTabBarHeader(this);
 		this.tabContentContainer = new DomTabBarContentContainer(this);
 		this.currentTab = null;
 		setCssClass(DomElementsCssClasses.DOM_TAB_BAR);
 		addCssClass(DomElementsCssClasses.DOM_TAB_BAR_HORIZONTAL);
 
-		appendChild(tabBarHeaderContainer);
+		appendChild(tabBarHeader);
 		appendChild(tabContentContainer);
 	}
 
@@ -64,7 +64,7 @@ public class DomTabBar extends DomDiv {
 		if (currentTab == null) {
 			showTab(tab);
 		} else {
-			tabBarHeaderContainer.refresh();
+			tabBarHeader.refresh(tabs);
 		}
 		return tab;
 	}
@@ -80,7 +80,7 @@ public class DomTabBar extends DomDiv {
 		}
 
 		currentTab = tab;
-		tabBarHeaderContainer.refresh();
+		tabBarHeader.refresh(tabs);
 		tabContentContainer.refresh();
 
 		currentTab.executeOnShowRefreshable();
@@ -89,23 +89,5 @@ public class DomTabBar extends DomDiv {
 	public DomTab getCurrentTab() {
 
 		return currentTab;
-	}
-
-	private class TabBarHeader extends DomDiv {
-
-		public TabBarHeader() {
-
-			setCssClass(DomElementsCssClasses.DOM_TAB_BAR_HEADER);
-		}
-
-		public void refresh() {
-
-			removeChildren();
-
-			for (DomTab tab: tabs) {
-				appendChild(new DomTabHeader(tab, DomTabBar.this));
-				appendChild(new DomTabHeaderSeparator());
-			}
-		}
 	}
 }
