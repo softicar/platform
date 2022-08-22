cd2c0dfd01a986c41e489e6d15b56057aa10cb18
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-08-22 21:24:55 +0200

    extracted style of DomAutoCompleteBackdrop to dom-auto-complete-style.css

--- platform-dom/src/main/java/com/softicar/platform/dom/elements/input/auto/DomAutoCompleteBackdrop.java
+++ platform-dom/src/main/java/com/softicar/platform/dom/elements/input/auto/DomAutoCompleteBackdrop.java
@@ -4,9 +4,6 @@ import com.softicar.platform.dom.elements.DomDiv;
 import com.softicar.platform.dom.elements.DomElementsCssClasses;
 import com.softicar.platform.dom.event.IDomClickEventHandler;
 import com.softicar.platform.dom.event.IDomEvent;
-import com.softicar.platform.dom.style.CssLength;
-import com.softicar.platform.dom.style.CssStyle;
-import com.softicar.platform.dom.styles.CssPosition;
 
 class DomAutoCompleteBackdrop extends DomDiv implements IDomClickEventHandler {
 
@@ -17,12 +14,6 @@ class DomAutoCompleteBackdrop extends DomDiv implements IDomClickEventHandler {
 		this.input = input;
 
 		addCssClass(DomElementsCssClasses.DOM_AUTO_COMPLETE_BACKDROP);
-
-		setStyle(CssPosition.FIXED);
-		setStyle(CssStyle.LEFT, CssLength.ZERO);
-		setStyle(CssStyle.TOP, CssLength.ZERO);
-		setStyle(CssStyle.RIGHT, CssLength.ZERO);
-		setStyle(CssStyle.BOTTOM, CssLength.ZERO);
 
 		getDomEngine().setPreventDefaultOnMouseDown(this, true);
 	}
