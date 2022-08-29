e15c3c2c12d57e039bd8b42bc91a02213feb3def
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-08-29 12:50:06 +0200

    add a test case for wheel event deduplication

--- platform-ajax/src/test/java/com/softicar/platform/ajax/dom/event/wheel/AjaxDomWheelEventTest.java
+++ platform-ajax/src/test/java/com/softicar/platform/ajax/dom/event/wheel/AjaxDomWheelEventTest.java
@@ -1,55 +1,155 @@
 package com.softicar.platform.ajax.dom.event.wheel;
 
-import com.softicar.platform.ajax.dom.event.AbstractAjaxDomEventTest;
 import com.softicar.platform.ajax.dom.event.AbstractAjaxDomEventTestDiv;
+import com.softicar.platform.ajax.testing.selenium.engine.level.low.AbstractAjaxSeleniumLowLevelTest;
+import com.softicar.platform.common.core.thread.sleep.Sleep;
 import com.softicar.platform.dom.event.DomEventType;
 import com.softicar.platform.dom.event.IDomEvent;
 import com.softicar.platform.dom.event.IDomWheelEventHandler;
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Iterator;
 import org.junit.Test;
 
-public class AjaxDomWheelEventTest extends AbstractAjaxDomEventTest {
-
-	public AjaxDomWheelEventTest() {
-
-		super(TestDiv::new);
-	}
+public class AjaxDomWheelEventTest extends AbstractAjaxSeleniumLowLevelTest {
 
 	@Test
 	public void testMouseWheelDown() {
+
+		var testDiv = openTestNode(() -> new TestDiv());
 
 		mouseWheelDown(testDiv);
 		waitForServer();
 
 		var events = testDiv.getEvents();
-		assertEquals(1, events.size());
-		var event = events.get(0);
-		assertEquals(DomEventType.WHEEL, event.getType());
-		assertEquals(0d, event.getDeltaX(), Double.MIN_VALUE);
-		assertTrue(event.getDeltaY() > 0);
-		assertEquals(0d, event.getDeltaZ(), Double.MIN_VALUE);
+		new WheelEventAsserter(events)//
+			.nextEvent()
+			.assertDeltaXZero()
+			.assertDeltaYPositive()
+			.assertDeltaZZero()
+			.assertNoMoreEvents();
 	}
 
 	@Test
 	public void testMouseWheelUp() {
 
+		var testDiv = openTestNode(() -> new TestDiv());
+
 		mouseWheelUp(testDiv);
 		waitForServer();
 
 		var events = testDiv.getEvents();
-		assertEquals(1, events.size());
-		var event = events.get(0);
-		assertEquals(DomEventType.WHEEL, event.getType());
-		assertEquals(0d, event.getDeltaX(), Double.MIN_VALUE);
-		assertTrue(event.getDeltaY() < 0);
-		assertEquals(0d, event.getDeltaZ(), Double.MIN_VALUE);
+		new WheelEventAsserter(events)//
+			.nextEvent()
+			.assertDeltaXZero()
+			.assertDeltaYNegative()
+			.assertDeltaZZero()
+			.assertNoMoreEvents();
+	}
+
+	@Test
+	public void testMouseWheelDeduplication() {
+
+		var testDiv = openTestNode(() -> new TestDiv().setSleepDuration(Duration.ofSeconds(2)));
+
+		mouseWheelDown(testDiv);
+		mouseWheelDown(testDiv);
+		mouseWheelDown(testDiv);
+		mouseWheelDown(testDiv);
+		mouseWheelUp(testDiv);
+		mouseWheelUp(testDiv);
+		mouseWheelDown(testDiv);
+		mouseWheelDown(testDiv);
+		waitForServer();
+
+		var events = testDiv.getEvents();
+		new WheelEventAsserter(events)//
+
+			.nextEvent()
+			.assertDeltaXZero()
+			.assertDeltaYPositive()
+			.assertDeltaZZero()
+
+			.nextEvent()
+			.assertDeltaXZero()
+			.assertDeltaYNegative()
+			.assertDeltaZZero()
+
+			// Expect no further "wheel down" event because it is swallowed by
+			// the current client-side implementation of wheel event deduplication.
+
+			.assertNoMoreEvents();
+	}
+
+	private static class WheelEventAsserter {
+
+		private final Iterator<IDomEvent> eventIterator;
+		private IDomEvent currentEvent;
+
+		public WheelEventAsserter(Collection<IDomEvent> events) {
+
+			this.eventIterator = events.iterator();
+			this.currentEvent = null;
+		}
+
+		public WheelEventAsserter assertDeltaXZero() {
+
+			assertEquals(0d, currentEvent.getDeltaX(), Double.MIN_VALUE);
+			return this;
+		}
+
+		public WheelEventAsserter assertDeltaYPositive() {
+
+			assertTrue(currentEvent.getDeltaY() > 0);
+			return this;
+		}
+
+		public WheelEventAsserter assertDeltaYNegative() {
+
+			assertTrue(currentEvent.getDeltaY() < 0);
+			return this;
+		}
+
+		public WheelEventAsserter assertDeltaZZero() {
+
+			assertEquals(0d, currentEvent.getDeltaZ(), Double.MIN_VALUE);
+			return this;
+		}
+
+		public WheelEventAsserter nextEvent() {
+
+			assertTrue(eventIterator.hasNext());
+			this.currentEvent = eventIterator.next();
+			assertEquals(DomEventType.WHEEL, currentEvent.getType());
+			return this;
+		}
+
+		public void assertNoMoreEvents() {
+
+			assertFalse(eventIterator.hasNext());
+		}
 	}
 
 	private static class TestDiv extends AbstractAjaxDomEventTestDiv implements IDomWheelEventHandler {
+
+		private Duration sleepDuration;
+
+		public TestDiv() {
+
+			this.sleepDuration = Duration.ZERO;
+		}
+
+		public TestDiv setSleepDuration(Duration sleepDuration) {
+
+			this.sleepDuration = sleepDuration;
+			return this;
+		}
 
 		@Override
 		public void handleWheel(IDomEvent event) {
 
 			addEvent(event);
+			Sleep.sleep(sleepDuration);
 		}
 	}
 }
