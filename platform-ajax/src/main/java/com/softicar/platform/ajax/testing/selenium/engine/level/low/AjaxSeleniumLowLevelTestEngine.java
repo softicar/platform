d51dad251ee63f0f4ff7475c8b85e6bcac68438c
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-09-16 13:19:09 +0200

    try waiting for out custom ready-flag

--- platform-ajax/src/main/java/com/softicar/platform/ajax/testing/selenium/engine/level/low/AjaxSeleniumLowLevelTestEngine.java
+++ platform-ajax/src/main/java/com/softicar/platform/ajax/testing/selenium/engine/level/low/AjaxSeleniumLowLevelTestEngine.java
@@ -7,15 +7,12 @@ import com.softicar.platform.ajax.testing.selenium.engine.level.high.AjaxSeleniu
 import com.softicar.platform.dom.DomProperties;
 import com.softicar.platform.dom.elements.testing.engine.IDomTestExecutionEngine;
 import com.softicar.platform.dom.node.IDomNode;
-import java.time.Duration;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import org.junit.Rule;
 import org.junit.runner.Description;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * A Selenium based UI test engine which can be used via a JUnit {@link Rule}
@@ -73,8 +70,7 @@ public class AjaxSeleniumLowLevelTestEngine extends AbstractAjaxSeleniumTestEngi
 		T testNode = testEnvironment.openTestNode(factory);
 
 		// TODO testing...
-		new WebDriverWait(webDriverController.getWebDriver(), Duration.ofSeconds(10))
-			.until(driver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete"));
+		waitForServer();
 
 		return testNode;
 	}
