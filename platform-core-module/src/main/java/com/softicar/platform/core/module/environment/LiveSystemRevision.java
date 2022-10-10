2c7e7f81f58e1962f570baf7663d3cbd151cb07e
Daniel Klose <daniel.klose@forspace-solutions.com>
2022-10-10 15:18:44 +0200

    PLAT-1157 Add Revision version to LiveSystemRevision

--- platform-core-module/src/main/java/com/softicar/platform/core/module/environment/LiveSystemRevision.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/environment/LiveSystemRevision.java
@@ -14,6 +14,7 @@ public class LiveSystemRevision {
 	private static final String BUILD_PROPERTIES_FILEPATH = "/release.properties";
 	private static final LiveSystemRevision CURRENT_REVISION = new LiveSystemRevision();
 	private String name = null;
+	private final String version = null;
 
 	/**
 	 * Returns an object describing the current revision of the project.
@@ -37,23 +38,36 @@ public class LiveSystemRevision {
 		return Optional.ofNullable(name);
 	}
 
+	/**
+	 * Returns an Optional with the version of the current revision, or an empty
+	 * Optional if the revision version could not be determined.
+	 *
+	 * @return an Optional with the version of the current revision, or an empty
+	 *         Optional
+	 */
+	public Optional<String> getVersion() {
+
+		return Optional.ofNullable(version);
+	}
+
 	private LiveSystemRevision() {
 
 		try (InputStream inputStream = LiveSystemRevision.class.getResourceAsStream(BUILD_PROPERTIES_FILEPATH)) {
 			if (inputStream != null) {
-				this.name = parseRevision(inputStream);
+				this.name = parseRevision(inputStream, "revision.id");
+				this.name = parseRevision(inputStream, "revision.version");
 			}
 		} catch (IOException exception) {
 			throw new SofticarIOException(exception);
 		}
 	}
 
-	private static String parseRevision(InputStream inputStream) {
+	private static String parseRevision(InputStream inputStream, String property) {
 
 		try (Reader reader = BufferedReaderFactory.readUtf8(inputStream)) {
 			Properties properties = new Properties();
 			properties.load(reader);
-			return properties.getProperty("revision.id");
+			return properties.getProperty(property);
 		} catch (Exception exception) {
 			DevNull.swallow(exception);
 			return null;
