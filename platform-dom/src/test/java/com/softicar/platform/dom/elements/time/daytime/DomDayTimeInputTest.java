cfd99107bbea67d5e1fff3b5f8f13fe827e5bb41
Daniel Klose <daniel.klose@forspace-solutions.com>
2022-10-13 10:23:16 +0200

    PLAT-1182-1 Rework DomTimeInput (#543)

--- platform-dom/src/test/java/com/softicar/platform/dom/elements/time/daytime/DomDayTimeInputTest.java
+++ platform-dom/src/test/java/com/softicar/platform/dom/elements/time/daytime/DomDayTimeInputTest.java
@@ -20,18 +20,18 @@ public class DomDayTimeInputTest extends AbstractDomValueInputDivTest<DayTime> {
 	public void testGetValue() {
 
 		// test empty input
-		assertEmptyResultForGetValue(",,,");
+		assertEmptyResultForGetValue(",");
 
 		// test valid dates and times
-		assertResultForGetValue("2021-12-15 08:04:02", "2021-12-15,8,4,2");
-		assertResultForGetValue("2021-12-15 23:59:59", "15.12.2021,23,59,59");
-		assertResultForGetValue("2021-12-15 00:00:00", "12/15/2021,0,0,0");
+		assertResultForGetValue("2021-12-15 08:04:02", "2021-12-15,8:4:2");
+		assertResultForGetValue("2021-12-15 23:59:59", "15.12.2021,23:59:59");
+		assertResultForGetValue("2021-12-15 00:00:00", "12/15/2021,0:0:0");
 
 		// test illegal combinations
-		assertExceptionForGetValue(CommonDateI18n.ILLEGAL_DATE_SPECIFICATION_ARG1.toDisplay("foo"), "foo,0,0,0");
-		assertExceptionForGetValue(CommonDateI18n.ILLEGAL_TIME_SPECIFICATION_ARG1.toDisplay("x:y:z"), "2021-01-01,x,y,z");
-		assertExceptionForGetValue(CommonDateI18n.MISSING_DATE_SPECIFICATION, ",0,0,0");
-		assertExceptionForGetValue(CommonDateI18n.MISSING_TIME_SPECIFICATION, "2021-01-01,,,");
+		assertExceptionForGetValue(CommonDateI18n.ILLEGAL_DATE_SPECIFICATION_ARG1.toDisplay("foo"), "foo,0:0:0");
+		assertExceptionForGetValue(CommonDateI18n.ILLEGAL_TIME_SPECIFICATION_ARG1.toDisplay("x:y:z"), "2021-01-01,x:y:z");
+		assertExceptionForGetValue(CommonDateI18n.MISSING_DATE_SPECIFICATION, ",0:0:0");
+		assertExceptionForGetValue(CommonDateI18n.MISSING_TIME_SPECIFICATION, "2021-01-01,");
 	}
 
 	@Override
@@ -40,7 +40,7 @@ public class DomDayTimeInputTest extends AbstractDomValueInputDivTest<DayTime> {
 		List<DomNodeTester> inputs = findNodes(IDomTextualInput.class).toList();
 
 		List<String> values = new Tokenizer(',', '\\').tokenize(valueText);
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 2; i++) {
 			inputs.get(i).setInputValue(values.get(i));
 		}
 	}
