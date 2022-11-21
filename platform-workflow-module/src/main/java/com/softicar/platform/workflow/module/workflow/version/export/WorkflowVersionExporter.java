4eb1204efb78edb560fdcbc5f3b40bad983b533a
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-11-21 09:33:03 +0100

    added current date to export name

--- platform-workflow-module/src/main/java/com/softicar/platform/workflow/module/workflow/version/export/WorkflowVersionExporter.java
+++ platform-workflow-module/src/main/java/com/softicar/platform/workflow/module/workflow/version/export/WorkflowVersionExporter.java
@@ -2,6 +2,7 @@ package com.softicar.platform.workflow.module.workflow.version.export;
 
 import com.google.gson.GsonBuilder;
 import com.softicar.platform.common.core.exceptions.SofticarIOException;
+import com.softicar.platform.common.date.Day;
 import com.softicar.platform.common.io.mime.MimeType;
 import com.softicar.platform.common.io.writer.ManagedWriter;
 import com.softicar.platform.common.io.writer.OutputStreamWriterFactory;
@@ -28,7 +29,7 @@ public class WorkflowVersionExporter {
 		var export = document//
 			.getEngine()
 			.createExport()
-			.setFilename(workflowVersion.getWorkflow().getName() + ".json")
+			.setFilename(workflowVersion.getWorkflow().getName() + "-" + Day.today().toYYMMDDString() + ".json")
 			.setMimeType(MimeType.APPLICATION_JSON)
 			.setCharset(Charsets.UTF8);
 		try (var stream = export.openOutputStream(); var writer = OutputStreamWriterFactory.writeUtf8(stream)) {
