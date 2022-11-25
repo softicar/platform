f465a9e6d38d60d9fadd28f698cca215ca4b5343
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2022-11-25 08:49:27 +0100

    PLAT-1230 Use H2 databases in case-insensitive mode (#597)

--- platform-db/src/test/java/com/softicar/platform/db/core/H2Test.java
+++ platform-db/src/test/java/com/softicar/platform/db/core/H2Test.java
@@ -5,17 +5,27 @@ import com.softicar.platform.db.core.connection.DbConnections;
 import com.softicar.platform.db.core.connection.IDbConnection;
 import com.softicar.platform.db.core.database.DbDatabases;
 import com.softicar.platform.db.core.statement.DbStatement;
+import java.util.List;
 import org.junit.Test;
 
 public class H2Test extends AbstractTest {
+
+	private final List<String> setupStatements;
+
+	public H2Test() {
+
+		this.setupStatements = List
+			.of(//
+				"CREATE SCHEMA test",
+				"CREATE TABLE test.test (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR)",
+				"INSERT INTO test.test (name) VALUES ('a'), ('b'), ('c')");
+	}
 
 	@Test
 	public void test() {
 
 		try (IDbConnection connection = DbConnections.getConnection(DbDatabases.H2_MEMORY_UNSHARED)) {
-			connection.executeUncached(new DbStatement("CREATE SCHEMA test"));
-			connection.executeUncached(new DbStatement("CREATE TABLE test.test (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR)"));
-			connection.executeUncached(new DbStatement("INSERT INTO test.test (name) VALUES ('a'), ('b'), ('c')"));
+			setupStatements.stream().map(DbStatement::new).forEach(connection::executeUncached);
 
 			try (DbResultSet resultSet = connection.executeQuery(new DbStatement("SELECT id, name FROM test.test ORDER BY id"))) {
 				assertTrue(resultSet.next());
@@ -26,6 +36,22 @@ public class H2Test extends AbstractTest {
 				assertEquals(2, resultSet.getInt("id"));
 				assertEquals("b", resultSet.getString("name"));
 
+				assertTrue(resultSet.next());
+				assertEquals(3, resultSet.getInt("id"));
+				assertEquals("c", resultSet.getString("name"));
+
+				assertFalse(resultSet.next());
+			}
+		}
+	}
+
+	@Test
+	public void testCaseInsensitivity() {
+
+		try (IDbConnection connection = DbConnections.getConnection(DbDatabases.H2_MEMORY_UNSHARED)) {
+			setupStatements.stream().map(DbStatement::new).forEach(connection::executeUncached);
+
+			try (DbResultSet resultSet = connection.executeQuery(new DbStatement("SELECT id, name FROM test.test WHERE name = 'C'"))) {
 				assertTrue(resultSet.next());
 				assertEquals(3, resultSet.getInt("id"));
 				assertEquals("c", resultSet.getString("name"));
