df2ff68625a361c563692d3da697a85ea9fc4908
Oliver Richers <oliver.richers@forspace-solutions.com>
2022-12-20 11:22:48 +0100

    PLAT-1234 Fixed Invisible Workflow Transition Elements

--- platform-workflow-module/src/main/java/com/softicar/platform/workflow/module/WorkflowCssClasses.java
+++ platform-workflow-module/src/main/java/com/softicar/platform/workflow/module/WorkflowCssClasses.java
@@ -7,6 +7,7 @@ public interface WorkflowCssClasses {
 
 	ICssClass WORKFLOW_MANAGEMENT_DIV = new CssClass("WorkflowManagementDiv", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
 	ICssClass WORKFLOW_DISPLAY_ELEMENT = new CssClass("WorkflowDisplayElement", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
+	ICssClass WORKFLOW_DISPLAY_LINE = new CssClass("WorkflowDisplayLine", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
 	ICssClass WORKFLOW_END_CIRCLE_INNER_CIRCLE = new CssClass("WorkflowEndCircleInnerCircle", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
 	ICssClass WORKFLOW_CIRCLE_DISPLAY_ELEMENT = new CssClass("WorkflowCircleDisplayElement", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
 	ICssClass WORKFLOW_NODE = new CssClass("WorkflowNode", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
