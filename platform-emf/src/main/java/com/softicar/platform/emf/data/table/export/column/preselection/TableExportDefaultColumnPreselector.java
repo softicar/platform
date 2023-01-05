e079a739a8e6d7ca20892ba6f0dc6fdff6070659
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2023-01-05 10:05:44 +0100

    EXP-2296-8 Table export: Unselect action columns by default

--- platform-emf/src/main/java/com/softicar/platform/emf/data/table/export/column/preselection/TableExportDefaultColumnPreselector.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/data/table/export/column/preselection/TableExportDefaultColumnPreselector.java
@@ -1,22 +1,41 @@
 package com.softicar.platform.emf.data.table.export.column.preselection;
 
-import com.softicar.platform.dom.elements.cell.separator.IDomSeparatorCell;
+import com.softicar.platform.dom.elements.cell.separator.DomSeparatorHeaderCell;
+import com.softicar.platform.emf.data.table.IEmfDataTableActionHeaderCell;
 import com.softicar.platform.emf.data.table.export.element.TableExportNamedDomCell;
 import java.util.Collection;
 
 public class TableExportDefaultColumnPreselector implements ITableExportColumnPreselector {
 
 	@Override
-	public boolean isPreselected(Collection<TableExportNamedDomCell> cells) {
+	public boolean isPreselected(Collection<TableExportNamedDomCell> namedCells) {
 
-		boolean preselected = true;
-
-		for (TableExportNamedDomCell namedCell: cells) {
-			if (namedCell != null && namedCell.getCell() instanceof IDomSeparatorCell) {
-				preselected = false;
-			}
+		if (firstCellIsSeparatorHeader(namedCells)) {
+			return false;
 		}
 
-		return preselected;
+		if (containsAnyActionHeaderCell(namedCells)) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private boolean containsAnyActionHeaderCell(Collection<TableExportNamedDomCell> namedCells) {
+
+		return namedCells//
+			.stream()
+			.map(TableExportNamedDomCell::getCell)
+			.anyMatch(IEmfDataTableActionHeaderCell.class::isInstance);
+	}
+
+	private boolean firstCellIsSeparatorHeader(Collection<TableExportNamedDomCell> namedCells) {
+
+		return namedCells//
+			.stream()
+			.findFirst()
+			.map(TableExportNamedDomCell::getCell)
+			.filter(DomSeparatorHeaderCell.class::isInstance)
+			.isPresent();
 	}
 }
