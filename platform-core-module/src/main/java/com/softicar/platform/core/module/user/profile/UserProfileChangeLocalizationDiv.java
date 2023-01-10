6b9e555fb1f3f55c973565a0c96f82933f960ad8
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2023-01-10 12:47:09 +0100

    PLAT-1260-3 Implement DomCheckboxGroup

--- platform-core-module/src/main/java/com/softicar/platform/core/module/user/profile/UserProfileChangeLocalizationDiv.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/user/profile/UserProfileChangeLocalizationDiv.java
@@ -2,25 +2,34 @@ package com.softicar.platform.core.module.user.profile;
 
 import com.softicar.platform.common.core.locale.CurrentLocale;
 import com.softicar.platform.core.module.CoreI18n;
-import com.softicar.platform.core.module.user.AGUser;
+import com.softicar.platform.core.module.localization.AGLocalization;
 import com.softicar.platform.core.module.user.CurrentUser;
 import com.softicar.platform.dom.elements.DomDiv;
 import com.softicar.platform.dom.elements.bar.DomActionBar;
 import com.softicar.platform.dom.elements.button.DomButton;
+import com.softicar.platform.dom.elements.checkbox.DomCheckboxGroup;
+import com.softicar.platform.dom.elements.label.DomLabelGrid;
 import com.softicar.platform.emf.EmfI18n;
-import com.softicar.platform.emf.editor.EmfAttributesDiv;
 
 class UserProfileChangeLocalizationDiv extends DomDiv {
 
-	private final EmfAttributesDiv<AGUser> attributesDiv;
+	private final DomCheckboxGroup<AGLocalization> localizationInput;
 
 	public UserProfileChangeLocalizationDiv() {
 
-		this.attributesDiv = new EmfAttributesDiv<>(CurrentUser.get(), true);
-		attributesDiv.addAttribute(AGUser.LOCALIZATION);
-
-		appendChild(attributesDiv);
+		this.localizationInput = new LocalizationInput();
+		var labelGrid = new DomLabelGrid().add(CoreI18n.LOCALIZATION, localizationInput);
+		appendChild(labelGrid);
 		appendChild(new DomActionBar(new SaveButton()));
+	}
+
+	private class LocalizationInput extends DomCheckboxGroup<AGLocalization> {
+
+		public LocalizationInput() {
+
+			AGLocalization.TABLE.loadAll().forEach(this::addOption);
+			setValue(CurrentUser.get().getLocalization());
+		}
 	}
 
 	private class SaveButton extends DomButton {
@@ -33,12 +42,11 @@ class UserProfileChangeLocalizationDiv extends DomDiv {
 
 		public void handleClick() {
 
-			if (attributesDiv.tryToApplyValidateAndSave()) {
-				CurrentLocale.set(CurrentUser.get().getLocale());
-				executeAlert(
-					CoreI18n.YOUR_LOCALIZATION_HAS_CHANGED//
-						.concatSentence(CoreI18n.PLEASE_PRESS_F5_TO_REFRESH));
-			}
+			CurrentUser.get().setLocalization(localizationInput.getValueOrThrow()).save();
+			CurrentLocale.set(CurrentUser.get().getLocale());
+			executeAlert(
+				CoreI18n.YOUR_LOCALIZATION_HAS_CHANGED//
+					.concatSentence(CoreI18n.PLEASE_PRESS_F5_TO_REFRESH));
 		}
 	}
 }
