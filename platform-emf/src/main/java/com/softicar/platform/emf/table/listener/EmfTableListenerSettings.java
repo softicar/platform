3bf66ef8c43a080a8e230d890b5b062398c489b0
Oliver Richers <oliver.richers@forspace-solutions.com>
2023-01-12 12:43:17 +0100

    added Javadoc to EmfTableListenerSettings

--- platform-emf/src/main/java/com/softicar/platform/emf/table/listener/EmfTableListenerSettings.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/table/listener/EmfTableListenerSettings.java
@@ -1,7 +1,13 @@
 package com.softicar.platform.emf.table.listener;
 
 import com.softicar.platform.db.core.connection.DbConnections;
+import com.softicar.platform.db.core.transaction.DbTransaction;
 
+/**
+ * Configures EMF specific settings for the current {@link DbTransaction}.
+ *
+ * @author Oliver Richers
+ */
 public class EmfTableListenerSettings {
 
 	private static final boolean DEFAULT_LOGGING_ENABLED = true;
@@ -12,6 +18,16 @@ public class EmfTableListenerSettings {
 		this.loggingEnabled = DEFAULT_LOGGING_ENABLED;
 	}
 
+	/**
+	 * Enables or disabled the writing of log records during the current
+	 * {@link DbTransaction}.
+	 * <p>
+	 * The default is <i>true</i>, thus log records will written by default.
+	 *
+	 * @param loggingEnabled
+	 *            <i>true</i> to enable writing of log records; <i>false</i> to
+	 *            disable writing of log records
+	 */
 	public static void setLoggingEnabled(boolean loggingEnabled) {
 
 		DbConnections//
@@ -19,6 +35,13 @@ public class EmfTableListenerSettings {
 			.ifPresent(settings -> settings.loggingEnabled = loggingEnabled);
 	}
 
+	/**
+	 * Returns whether log records shall be written during the current
+	 * {@link DbTransaction}.
+	 *
+	 * @return <i>true</i> if log records shall be written; <i>false</i>
+	 *         otherwise
+	 */
 	public static boolean isLoggingEnabled() {
 
 		return DbConnections//
