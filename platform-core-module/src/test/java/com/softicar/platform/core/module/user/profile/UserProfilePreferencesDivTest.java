softicar/platform

commit d6fcb9f10c8f925c46dc643b2c7538dc70888c46

PLAT-1260-1 Store user preferences in a JSON field (#636)

platform-core-module/src/test/java/com/softicar/platform/core/module/user/profile/UserProfilePreferencesDivTest.java
@@ -1,0 +1,91 @@
+package com.softicar.platform.core.module.user.profile;
+
+import com.softicar.platform.core.module.CoreTestMarker;
+import com.softicar.platform.core.module.test.AbstractCoreTest;
+import com.softicar.platform.core.module.user.CurrentUser;
+import com.softicar.platform.core.module.user.UserPreferences;
+import org.junit.Test;
+
+public class UserProfilePreferencesDivTest extends AbstractCoreTest {
+
+	public UserProfilePreferencesDivTest() {
+
+		setNodeSupplier(UserProfilePreferencesDiv::new);
+	}
+
+	@Test
+	public void testInitialState() {
+
+		var preferences = new UserPreferences();
+		preferences.automaticallyCollapseFolders = true;
+		preferences.recursivelyCollapseFolders = true;
+		CurrentUser.get().savePreferences(preferences);
+
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(true).assertNodeDisabled(false);
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(true).assertNodeDisabled(true);
+		assertTrue(getUserPreferences().automaticallyCollapseFolders);
+		assertTrue(getUserPreferences().recursivelyCollapseFolders);
+	}
+
+	@Test
+	public void testInitialStateWithDefaults() {
+
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(false).assertNodeDisabled(false);
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(false).assertNodeDisabled(false);
+		assertFalse(getUserPreferences().automaticallyCollapseFolders);
+		assertFalse(getUserPreferences().recursivelyCollapseFolders);
+	}
+
+	@Test
+	public void testWithClickOnAutomaticallyCollapseFoldersCheckbox() {
+
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).click();
+
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(true).assertNodeDisabled(false);
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(true).assertNodeDisabled(true);
+		assertFalse(getUserPreferences().automaticallyCollapseFolders);
+		assertFalse(getUserPreferences().recursivelyCollapseFolders);
+
+		findButton(CoreTestMarker.USER_PREFERENCES_SAVE_BUTTON).click();
+
+		assertTrue(getUserPreferences().automaticallyCollapseFolders);
+		assertTrue(getUserPreferences().recursivelyCollapseFolders);
+	}
+
+	@Test
+	public void testWithClickOnAutomaticallyCollapseFoldersCheckboxTwice() {
+
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).click().click();
+
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(false).assertNodeDisabled(false);
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(true).assertNodeDisabled(false);
+		assertFalse(getUserPreferences().automaticallyCollapseFolders);
+		assertFalse(getUserPreferences().recursivelyCollapseFolders);
+
+		findButton(CoreTestMarker.USER_PREFERENCES_SAVE_BUTTON).click();
+
+		assertFalse(getUserPreferences().automaticallyCollapseFolders);
+		assertTrue(getUserPreferences().recursivelyCollapseFolders);
+	}
+
+	@Test
+	public void testWithClickOnRecursivelyCollapseFoldersCheckbox() {
+
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).click();
+
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(false).assertNodeDisabled(false);
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(true).assertNodeDisabled(false);
+		assertFalse(getUserPreferences().automaticallyCollapseFolders);
+		assertFalse(getUserPreferences().recursivelyCollapseFolders);
+
+		findButton(CoreTestMarker.USER_PREFERENCES_SAVE_BUTTON).click();
+
+		assertFalse(getUserPreferences().automaticallyCollapseFolders);
+		assertTrue(getUserPreferences().recursivelyCollapseFolders);
+	}
+
+	private UserPreferences getUserPreferences() {
+
+		return CurrentUser.get().getPreferences();
+	}
+}

platform-core-module/src/main/java/com/softicar/platform/core/module/user/UserPreferences.java
@@ -1,0 +1,15 @@
+package com.softicar.platform.core.module.user;
+
+/**
+ * Provides user specific preferences.
+ * <p>
+ * <b>WARNING:</b> Instances are serialized and un-serialized via Gson. Renaming
+ * fields in here will partially invalidate stored user preferences.
+ *
+ * @author Alexander Schmidt
+ */
+public class UserPreferences {
+
+	public boolean automaticallyCollapseFolders = false;
+	public boolean recursivelyCollapseFolders = false;
+}

platform-core-module/src/main/java/com/softicar/platform/core/module/CoreI18n.java
@@ -696,6 +696,10 @@ public interface CoreI18n extends EmfI18n {
 		.de("Portal-Logo");
 	I18n0 PORTAL_PROTOCOL = new I18n0("Portal Protocol")//
 		.de("Portal-Protokoll");
+	I18n0 PREFERENCES = new I18n0("Preferences")//
+		.de("Einstellungen");
+	I18n0 PREFERENCES_JSON = new I18n0("Preferences JSON")//
+		.de("Einstellungen JSON");
 	I18n0 PREFERENCES_SAVED = new I18n0("Preferences saved.")//
 		.de("Einstellungen gespeichert.");
 	I18n0 PRIMARY_FILE_SERVER = new I18n0("Primary File Server")//

platform-core-module/src/main/java/com/softicar/platform/core/module/CoreTestMarker.java
@@ -24,4 +24,8 @@ public enum CoreTestMarker implements ITestMarker {
 
 	STORED_FILE_DOWNLOAD_FILE_BUTTON,
 	STORED_FILE_REMOVE_FILE_BUTTON,
+
+	USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX,
+	USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX,
+	USER_PREFERENCES_SAVE_BUTTON,
 }

platform-core-module/src/main/java/com/softicar/platform/core/module/page/navigation/PageNavigationController.java
@@ -103,7 +103,7 @@ public class PageNavigationController {
 			desiredFolderDivs.add(folderDiv);
 			folderDiv = folderDiv.getParentFolderDiv();
 		}
-		if (CurrentUser.get().isAutomaticallyCollapseFolders()) {
+		if (CurrentUser.get().getPreferences().automaticallyCollapseFolders) {
 			Sets.difference(openFolderDivs, desiredFolderDivs).forEach(this::close);
 		}
 		Sets.difference(desiredFolderDivs, openFolderDivs).forEach(this::open);
@@ -117,7 +117,7 @@ public class PageNavigationController {
 
 	private void close(PageNavigationFolderDiv folderDiv) {
 
-		new FolderCloser(CurrentUser.get().isRecursivelyCollapseFolders()).close(folderDiv);
+		new FolderCloser(CurrentUser.get().getPreferences().recursivelyCollapseFolders).close(folderDiv);
 	}
 
 	private void openStartPageWithAlert(IDisplayString message) {

platform-core-module/src/main/java/com/softicar/platform/core/module/test/fixture/CoreModuleTestFixture.java
@@ -49,17 +49,14 @@ public class CoreModuleTestFixture implements IModuleTestFixture<AGCoreModuleIns
 
 		this.viewUser = insertUser("View", "User")//
 			.setEmailAddress("view.user@example.com")
-			.setAutomaticallyCollapseFolders(false)
 			.save();
 
 		this.normalUser = insertUser("Normal", "User")//
 			.setEmailAddress("normal.user@example.com")
-			.setAutomaticallyCollapseFolders(false)
 			.save();
 
 		this.adminUser = insertUser("Admin", "User")//
 			.setEmailAddress("admin.user@example.com")
-			.setAutomaticallyCollapseFolders(false)
 			.save();
 
 		insertPassword(viewUser, "test");

platform-core-module/src/main/java/com/softicar/platform/core/module/user/AGUser.java
@@ -1,11 +1,14 @@
 package com.softicar.platform.core.module.user;
 
+import com.google.gson.Gson;
 import com.softicar.platform.common.core.exceptions.SofticarException;
 import com.softicar.platform.common.core.i18n.DisplayString;
 import com.softicar.platform.common.core.i18n.IDisplayString;
 import com.softicar.platform.common.core.locale.ILocale;
 import com.softicar.platform.common.core.locale.LocaleScope;
+import com.softicar.platform.common.core.logging.Log;
 import com.softicar.platform.common.core.user.IBasicUser;
+import com.softicar.platform.common.core.utils.DevNull;
 import com.softicar.platform.common.date.DayTime;
 import com.softicar.platform.core.module.AGCoreModuleInstance;
 import com.softicar.platform.core.module.CoreI18n;
@@ -25,7 +28,9 @@ import com.softicar.platform.emf.module.permission.IEmfModulePermission;
 import com.softicar.platform.emf.object.IEmfObject;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public class AGUser extends AGUserGenerated implements IEmfObject<AGUser>, IBasicUser {
 
@@ -246,5 +251,61 @@ public class AGUser extends AGUserGenerated implements IEmfObject<AGUser>, IBasi
 	public IDisplayString toDisplay() {
 
 		return toDisplayWithoutId();
+	}
+
+	/**
+	 * Derives the current {@link UserPreferences} from the current value of
+	 * {@link #PREFERENCES_JSON}.
+	 * <p>
+	 * If the processed JSON value is empty or invalid, a
+	 * {@link UserPreferences} instance with default values is returned.
+	 *
+	 * @return the {@link UserPreferences} (never <i>null</i>)
+	 */
+	public UserPreferences getPreferences() {
+
+		return getPreferencesFromJson().orElse(new UserPreferences());
+	}
+
+	/**
+	 * Saves the given {@link UserPreferences} for this {@link AGUser}.
+	 *
+	 * @param preferences
+	 *            the {@link UserPreferences} to save (never <i>null</i>)
+	 * @return this
+	 */
+	public AGUser savePreferences(UserPreferences preferences) {
+
+		Objects.requireNonNull(preferences);
+		String preferencesJson = new Gson().toJson(preferences);
+		setPreferencesJson(preferencesJson).save();
+		return this;
+	}
+
+	/**
+	 * Fetches the current {@link UserPreferences}, modifies them with the given
+	 * {@link Consumer}, and saves the result.
+	 *
+	 * @param preferencesModifier
+	 *            modifies the loaded {@link UserPreferences} (never
+	 *            <i>null</i>)
+	 * @return this
+	 */
+	public AGUser updatePreferences(Consumer<UserPreferences> preferencesModifier) {
+
+		var preferences = getPreferences();
+		preferencesModifier.accept(preferences);
+		return savePreferences(preferences);
+	}
+
+	private Optional<UserPreferences> getPreferencesFromJson() {
+
+		try {
+			return Optional.ofNullable(new Gson().fromJson(getPreferencesJson(), UserPreferences.class));
+		} catch (Exception exception) {
+			DevNull.swallow(exception);
+			Log.ferror("Failed to retrieve preferences for user '%s'.", toDisplay());
+			return Optional.empty();
+		}
 	}
 }

platform-core-module/src/main/java/com/softicar/platform/core/module/user/AGUser.sqml
@@ -13,10 +13,9 @@ TABLE AGUser 'Core.User' {
 	String emailAddress = '' [MAXLENGTH=255]
 	Boolean systemUser = FALSE
 	AGLocalization localization [CONSTRAINT_NAME='User_ibfk_1']
-	Boolean automaticallyCollapseFolders = TRUE
-	Boolean recursivelyCollapseFolders = FALSE
 	AGPasswordPolicy passwordPolicy = NULL [NULLABLE CONSTRAINT_NAME='User_ibfk_2']
 	AGUserAllowedIpRule allowedIpRule = NULL [NULLABLE CONSTRAINT_NAME='User_ibfk_3']
+	String preferencesJson = '' [LENGTHBITS=16]
 
 	UK loginName
 	IK localization

commit 4a105ecc3ba4386124f397d633139288595829ec

PLAT-1267 Fix default navigation behavior
This fixes an unintended change in navigation folder collapsing default behavior, caused by #636 / `PLAT-1260-1`.

- Navigation folders will now collapse again by default, for new users.
- Note: Even before #636 the default values (`automatic=true` & `recursive=false`) were inconsistent, i.e. they specified the sole invalid combination of modes.

platform-core-module/src/main/java/com/softicar/platform/core/module/user/preferences/UserPreferences.java
@@ -10,7 +10,7 @@ package com.softicar.platform.core.module.user.preferences;
  */
 public class UserPreferences {
 
-	public boolean automaticallyCollapseFolders = false;
-	public boolean recursivelyCollapseFolders = false;
+	public boolean automaticallyCollapseFolders = true;
+	public boolean recursivelyCollapseFolders = true;
 	public UserPreferencesPreferredPopupPlacement preferredPopupPlacement = UserPreferencesPreferredPopupPlacement.AT_EVENT_COORDINATES;
 }

platform-core-module/src/test/java/com/softicar/platform/core/module/user/preferences/UserPreferencesManagerTest.java
@@ -21,15 +21,15 @@ public class UserPreferencesManagerTest extends AbstractCoreTest {
 
 		// setup
 		user.setPreferencesJson("""
-				{"automaticallyCollapseFolders":true,"recursivelyCollapseFolders":true,"preferredPopupPlacement":"CENTERED"}
+				{"automaticallyCollapseFolders":false,"recursivelyCollapseFolders":false,"preferredPopupPlacement":"CENTERED"}
 				""").save();
 
 		// execute
 		var preferences = manager.getPreferences();
 
 		// assert results
-		assertTrue(preferences.automaticallyCollapseFolders);
-		assertTrue(preferences.recursivelyCollapseFolders);
+		assertFalse(preferences.automaticallyCollapseFolders);
+		assertFalse(preferences.recursivelyCollapseFolders);
 		assertEquals(UserPreferencesPreferredPopupPlacement.CENTERED, preferences.preferredPopupPlacement);
 	}
 
@@ -43,8 +43,8 @@ public class UserPreferencesManagerTest extends AbstractCoreTest {
 		var preferences = manager.getPreferences();
 
 		// assert results
-		assertFalse(preferences.automaticallyCollapseFolders);
-		assertFalse(preferences.recursivelyCollapseFolders);
+		assertTrue(preferences.automaticallyCollapseFolders);
+		assertTrue(preferences.recursivelyCollapseFolders);
 		assertEquals(UserPreferencesPreferredPopupPlacement.AT_EVENT_COORDINATES, preferences.preferredPopupPlacement);
 	}
 
@@ -58,8 +58,8 @@ public class UserPreferencesManagerTest extends AbstractCoreTest {
 		var preferences = manager.getPreferences();
 
 		// assert results
-		assertFalse(preferences.automaticallyCollapseFolders);
-		assertFalse(preferences.recursivelyCollapseFolders);
+		assertTrue(preferences.automaticallyCollapseFolders);
+		assertTrue(preferences.recursivelyCollapseFolders);
 		assertEquals(UserPreferencesPreferredPopupPlacement.AT_EVENT_COORDINATES, preferences.preferredPopupPlacement);
 	}
 
@@ -92,8 +92,8 @@ public class UserPreferencesManagerTest extends AbstractCoreTest {
 
 		// setup
 		var preferences = new UserPreferences();
-		preferences.automaticallyCollapseFolders = true;
-		preferences.recursivelyCollapseFolders = true;
+		preferences.automaticallyCollapseFolders = false;
+		preferences.recursivelyCollapseFolders = false;
 		preferences.preferredPopupPlacement = UserPreferencesPreferredPopupPlacement.CENTERED;
 
 		// execute
@@ -101,8 +101,8 @@ public class UserPreferencesManagerTest extends AbstractCoreTest {
 
 		// assert results
 		assertFalse(user.getPreferencesJson().isEmpty());
-		assertTrue(manager.getPreferences().automaticallyCollapseFolders);
-		assertTrue(manager.getPreferences().recursivelyCollapseFolders);
+		assertFalse(manager.getPreferences().automaticallyCollapseFolders);
+		assertFalse(manager.getPreferences().recursivelyCollapseFolders);
 		assertEquals(UserPreferencesPreferredPopupPlacement.CENTERED, manager.getPreferences().preferredPopupPlacement);
 	}
 

platform-core-module/src/test/java/com/softicar/platform/core/module/user/profile/UserProfilePreferencesDivTest.java
@@ -18,32 +18,37 @@ public class UserProfilePreferencesDivTest extends AbstractCoreTest {
 	public void testInitialState() {
 
 		var preferences = new UserPreferences();
-		preferences.automaticallyCollapseFolders = true;
-		preferences.recursivelyCollapseFolders = true;
+		preferences.automaticallyCollapseFolders = false;
+		preferences.recursivelyCollapseFolders = false;
 		preferences.preferredPopupPlacement = UserPreferencesPreferredPopupPlacement.CENTERED;
 		CurrentUser.get().savePreferences(preferences);
 
-		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(true).assertNodeDisabled(false);
-		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(true).assertNodeDisabled(true);
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(false).assertNodeDisabled(false);
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(false).assertNodeDisabled(false);
 		findCheckboxGroup(CoreTestMarker.USER_PREFERENCES_PREFERRED_POPUP_PLACEMENT).assertValue(UserPreferencesPreferredPopupPlacement.CENTERED);
-		assertTrue(getUserPreferences().automaticallyCollapseFolders);
-		assertTrue(getUserPreferences().recursivelyCollapseFolders);
+		assertFalse(getUserPreferences().automaticallyCollapseFolders);
+		assertFalse(getUserPreferences().recursivelyCollapseFolders);
 		assertEquals(UserPreferencesPreferredPopupPlacement.CENTERED, getUserPreferences().preferredPopupPlacement);
 	}
 
 	@Test
 	public void testInitialStateWithDefaults() {
 
-		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(false).assertNodeDisabled(false);
-		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(false).assertNodeDisabled(false);
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(true).assertNodeDisabled(false);
+		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).assertChecked(true).assertNodeDisabled(true);
 		findCheckboxGroup(CoreTestMarker.USER_PREFERENCES_PREFERRED_POPUP_PLACEMENT).assertValue(UserPreferencesPreferredPopupPlacement.AT_EVENT_COORDINATES);
-		assertFalse(getUserPreferences().automaticallyCollapseFolders);
-		assertFalse(getUserPreferences().recursivelyCollapseFolders);
+		assertTrue(getUserPreferences().automaticallyCollapseFolders);
+		assertTrue(getUserPreferences().recursivelyCollapseFolders);
 		assertEquals(UserPreferencesPreferredPopupPlacement.AT_EVENT_COORDINATES, getUserPreferences().preferredPopupPlacement);
 	}
 
 	@Test
-	public void testWithClickOnAutomaticallyCollapseFoldersCheckbox() {
+	public void testWithTogglingAutomaticallyCollapseFolders() {
+
+		CurrentUser.get().updatePreferences(preferences -> {
+			preferences.automaticallyCollapseFolders = false;
+			preferences.recursivelyCollapseFolders = false;
+		});
 
 		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).click();
 
@@ -59,7 +64,12 @@ public class UserProfilePreferencesDivTest extends AbstractCoreTest {
 	}
 
 	@Test
-	public void testWithClickOnAutomaticallyCollapseFoldersCheckboxTwice() {
+	public void testWithTogglingAutomaticallyCollapseFoldersAgain() {
+
+		CurrentUser.get().updatePreferences(preferences -> {
+			preferences.automaticallyCollapseFolders = false;
+			preferences.recursivelyCollapseFolders = false;
+		});
 
 		findCheckbox(CoreTestMarker.USER_PREFERENCES_AUTOMATICALLY_COLLAPSE_FOLDERS_CHECKBOX).click().click();
 
@@ -75,7 +85,12 @@ public class UserProfilePreferencesDivTest extends AbstractCoreTest {
 	}
 
 	@Test
-	public void testWithClickOnRecursivelyCollapseFoldersCheckbox() {
+	public void testWithTogglingRecursivelyCollapseFolders() {
+
+		CurrentUser.get().updatePreferences(preferences -> {
+			preferences.automaticallyCollapseFolders = false;
+			preferences.recursivelyCollapseFolders = false;
+		});
 
 		findCheckbox(CoreTestMarker.USER_PREFERENCES_RECURSIVELY_COLLAPSE_FOLDERS_CHECKBOX).click();
 