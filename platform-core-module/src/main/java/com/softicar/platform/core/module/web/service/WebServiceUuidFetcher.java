6b472ff2ada25b6f72c0ad3b8a7d4a9504627d9f
Oliver Richers <oliver.richers@forspace-solutions.com>
2023-02-08 08:48:55 +0100

    PLAT-1314 Support for Legacy Service URLs

--- platform-core-module/src/main/java/com/softicar/platform/core/module/web/service/WebServiceUuidFetcher.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/web/service/WebServiceUuidFetcher.java
@@ -1,6 +1,9 @@
 package com.softicar.platform.core.module.web.service;
 
+import com.softicar.platform.common.core.logging.Log;
+import com.softicar.platform.common.core.utils.DevNull;
 import com.softicar.platform.common.network.http.error.HttpBadRequestError;
+import java.util.Optional;
 import java.util.UUID;
 import javax.servlet.http.HttpServletRequest;
 
@@ -15,10 +18,30 @@ public class WebServiceUuidFetcher {
 
 	public UUID getServiceUuidOrThrow() {
 
+		return getFromParameter().orElseGet(this::getFromPath);
+	}
+
+	private UUID getFromPath() {
+
 		String serviceIdentifier = new WebServicePathExtractor(request)//
 			.extractPathOrThrow()
 			.getServiceIdentifier();
 		return parseUuid(serviceIdentifier);
+	}
+
+	// only necessary for temporary backwards compatibility
+	private Optional<UUID> getFromParameter() {
+
+		String idParameter = request.getParameter("id");
+		if (idParameter != null) {
+			try {
+				return Optional.of(UUID.fromString(idParameter));
+			} catch (Exception exception) {
+				DevNull.swallow(exception);
+				Log.fwarning("Got id request parameter but failed to parse it into UUID.");
+			}
+		}
+		return Optional.empty();
 	}
 
 	private UUID parseUuid(String uuidString) {
