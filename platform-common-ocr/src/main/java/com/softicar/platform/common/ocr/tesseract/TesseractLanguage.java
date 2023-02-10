cc54a309aaedf2829168b76cfcb14539b1262315
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2023-02-10 18:04:43 +0100

    PLAT-1316 platform-common-ocr: Rework package structure and visibilities (#718)

--- platform-common-ocr/src/main/java/com/softicar/platform/common/ocr/tesseract/TesseractLanguage.java
+++ platform-common-ocr/src/main/java/com/softicar/platform/common/ocr/tesseract/TesseractLanguage.java
@@ -1,8 +1,6 @@
 package com.softicar.platform.common.ocr.tesseract;
 
 import com.softicar.platform.common.io.resource.IResource;
-import com.softicar.platform.common.ocr.tesseract.trained.data.TesseractTrainedDataFileResourceContainer;
-import com.softicar.platform.common.ocr.tesseract.trained.data.TesseractTrainedDataFiles;
 
 /**
  * Enumerates supported document languages for {@link TesseractOcrTextExtractor}
