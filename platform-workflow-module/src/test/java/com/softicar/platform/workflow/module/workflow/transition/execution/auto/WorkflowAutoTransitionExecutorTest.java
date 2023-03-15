ba247e8c0b993f2084449b312aecebd286988c01
Thees Koester <thees.koester@forspace-solutions.com>
2023-03-15 11:08:15 +0100

    Fix error in WorkflowAutoTransitionExecutorTest

--- platform-workflow-module/src/test/java/com/softicar/platform/workflow/module/workflow/transition/execution/auto/WorkflowAutoTransitionExecutorTest.java
+++ platform-workflow-module/src/test/java/com/softicar/platform/workflow/module/workflow/transition/execution/auto/WorkflowAutoTransitionExecutorTest.java
@@ -8,6 +8,7 @@ import com.softicar.platform.workflow.module.workflow.node.AGWorkflowNode;
 import com.softicar.platform.workflow.module.workflow.transition.AGWorkflowTransition;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import org.junit.Test;
 
 public class WorkflowAutoTransitionExecutorTest extends AbstractTestObjectWorkflowTest {
