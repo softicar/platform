9baa68ea9cebdfb77771a1f0caeea07dd5eccd09
Oliver Richers <oliver.richers@forspace-solutions.com>
2023-03-30 14:06:23 +0200

    PLAT-1345-2 Showing Current Workflow Version Table Row in Bold (#754)

--- platform-workflow-module/src/main/java/com/softicar/platform/workflow/module/WorkflowCssClasses.java
+++ platform-workflow-module/src/main/java/com/softicar/platform/workflow/module/WorkflowCssClasses.java
@@ -5,19 +5,14 @@ import com.softicar.platform.dom.style.ICssClass;
 
 public interface WorkflowCssClasses {
 
-	ICssClass WORKFLOW_MANAGEMENT_DIV = new CssClass("WorkflowManagementDiv", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
+	ICssClass WORKFLOW_CIRCLE_DISPLAY_ELEMENT = new CssClass("WorkflowCircleDisplayElement", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
 	ICssClass WORKFLOW_DISPLAY_ELEMENT = new CssClass("WorkflowDisplayElement", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
 	ICssClass WORKFLOW_DISPLAY_LINE = new CssClass("WorkflowDisplayLine", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
-	ICssClass WORKFLOW_END_CIRCLE_INNER_CIRCLE = new CssClass("WorkflowEndCircleInnerCircle", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
-	ICssClass WORKFLOW_CIRCLE_DISPLAY_ELEMENT = new CssClass("WorkflowCircleDisplayElement", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
-	ICssClass WORKFLOW_NODE = new CssClass("WorkflowNode", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
-	ICssClass WORKFLOW_NODE_SEPARATOR = new CssClass("WorkflowNodeSeparator", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
-	ICssClass WORKFLOW_NODE_NAME = new CssClass("WorkflowNodeName", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
 	ICssClass WORKFLOW_DISPLAY_LINE_PIXEL = new CssClass("WorkflowDisplayLinePixel", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
+	ICssClass WORKFLOW_END_CIRCLE_INNER_CIRCLE = new CssClass("WorkflowEndCircleInnerCircle", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
 
 	ICssClass WORKFLOW_ITEM_COUNT_IN_INAVTICE_VERSIONS_CELL =
 			new CssClass("WorkflowItemCountInInavticeVersionsCell", WorkflowCssFiles.WORKFLOW_ITEM_HISTORY_STYLE);
-
 	ICssClass WORKFLOW_ITEM_HISTORY_DIV = new CssClass("WorkflowItemHistoryDiv", WorkflowCssFiles.WORKFLOW_ITEM_HISTORY_STYLE);
 	ICssClass WORKFLOW_ITEM_HISTORY_ENTRY_DATE = new CssClass("WorkflowItemHistoryEntryDate", WorkflowCssFiles.WORKFLOW_ITEM_HISTORY_STYLE);
 	ICssClass WORKFLOW_ITEM_HISTORY_ENTRY_DIV = new CssClass("WorkflowItemHistoryEntryDiv", WorkflowCssFiles.WORKFLOW_ITEM_HISTORY_STYLE);
@@ -26,4 +21,10 @@ public interface WorkflowCssClasses {
 	ICssClass WORKFLOW_ITEM_HISTORY_SHOW_MORE_DIV = new CssClass("WorkflowItemHistoryShowMoreDiv", WorkflowCssFiles.WORKFLOW_ITEM_HISTORY_STYLE);
 	ICssClass WORKFLOW_ITEM_HISTORY_WORKFLOW_NODE_DIV = new CssClass("WorkflowItemHistoryWorkflowNodeDiv", WorkflowCssFiles.WORKFLOW_ITEM_HISTORY_STYLE);
 	ICssClass WORKFLOW_ITEM_HISTORY_WORKFLOW_NODE_LABEL = new CssClass("WorkflowItemHistoryWorkflowNodeLabel", WorkflowCssFiles.WORKFLOW_ITEM_HISTORY_STYLE);
+
+	ICssClass WORKFLOW_MANAGEMENT_DIV = new CssClass("WorkflowManagementDiv", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
+	ICssClass WORKFLOW_NODE = new CssClass("WorkflowNode", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
+	ICssClass WORKFLOW_NODE_SEPARATOR = new CssClass("WorkflowNodeSeparator", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
+	ICssClass WORKFLOW_NODE_NAME = new CssClass("WorkflowNodeName", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
+	ICssClass WORKFLOW_VERSION_ROW = new CssClass("WorkflowVersionRow", WorkflowCssFiles.WORKFLOW_DISPLAY_ELEMENTS_STYLE);
 }
--- platform-workflow-module/src/main/java/com/softicar/platform/workflow/module/workflow/version/AGWorkflowVersionTable.java
+++ platform-workflow-module/src/main/java/com/softicar/platform/workflow/module/workflow/version/AGWorkflowVersionTable.java
@@ -2,11 +2,13 @@ package com.softicar.platform.workflow.module.workflow.version;
 
 import com.softicar.platform.common.container.comparator.OrderDirection;
 import com.softicar.platform.db.runtime.object.IDbObjectTableBuilder;
+import com.softicar.platform.dom.DomCssPseudoClasses;
 import com.softicar.platform.emf.action.EmfActionSet;
 import com.softicar.platform.emf.attribute.EmfAttributeReorderer;
 import com.softicar.platform.emf.attribute.IEmfAttributeList;
 import com.softicar.platform.emf.attribute.field.bool.EmfBooleanDisplay;
 import com.softicar.platform.emf.authorizer.EmfAuthorizer;
+import com.softicar.platform.emf.data.table.IEmfDataTableRow;
 import com.softicar.platform.emf.form.tab.factory.EmfFormTabConfiguration;
 import com.softicar.platform.emf.log.EmfChangeLoggerSet;
 import com.softicar.platform.emf.management.EmfManagementConfiguration;
@@ -14,6 +16,7 @@ import com.softicar.platform.emf.mapper.IEmfTableRowMapper;
 import com.softicar.platform.emf.object.table.EmfObjectTable;
 import com.softicar.platform.emf.predicate.EmfPredicates;
 import com.softicar.platform.emf.table.configuration.EmfTableConfiguration;
+import com.softicar.platform.workflow.module.WorkflowCssClasses;
 import com.softicar.platform.workflow.module.WorkflowI18n;
 import com.softicar.platform.workflow.module.WorkflowImages;
 import com.softicar.platform.workflow.module.WorkflowPermissions;
@@ -48,6 +51,7 @@ public class AGWorkflowVersionTable extends EmfObjectTable<AGWorkflowVersion, AG
 	public void customizeManagementConfiguraton(EmfManagementConfiguration<AGWorkflowVersion> configuration) {
 
 		configuration.addOrderBy(AGWorkflowVersion.ID, OrderDirection.DESCENDING);
+		configuration.setRowCustomizer(this::customizeRow);
 	}
 
 	@Override
@@ -79,6 +83,9 @@ public class AGWorkflowVersionTable extends EmfObjectTable<AGWorkflowVersion, AG
 		reorderer//
 			.moveAttribute(AGWorkflowVersion.CURRENT_VERSION_FIELD)
 			.inFrontOf(AGWorkflowVersion.DRAFT);
+		reorderer//
+			.moveAttribute(AGWorkflowVersion.ROOT_NODE)
+			.toBack();
 	}
 
 	@Override
@@ -104,5 +111,13 @@ public class AGWorkflowVersionTable extends EmfObjectTable<AGWorkflowVersion, AG
 			.addPlainChangeLogger(AGWorkflowVersionLog.WORKFLOW_VERSION, AGWorkflowVersionLog.TRANSACTION)//
 			.addMapping(AGWorkflowVersion.ROOT_NODE, AGWorkflowVersionLog.ROOT_NODE)
 			.addMapping(AGWorkflowVersion.DRAFT, AGWorkflowVersionLog.DRAFT);
+	}
+
+	private void customizeRow(IEmfDataTableRow<AGWorkflowVersion> row) {
+
+		row.addCssClass(WorkflowCssClasses.WORKFLOW_VERSION_ROW);
+		if (row.getDataRow().isCurrentVersion()) {
+			row.addCssClass(DomCssPseudoClasses.ACTIVE);
+		}
 	}
 }
