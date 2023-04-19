8a9751c646b7c26255d9fd70349b5bc607d10907
Alexander Schmidt <alexander.schmidt@forspace-solutions.com>
2023-04-19 12:01:31 +0200

    PLAT-1359 BufferedEmailSenderSessionManager: Use finite timeouts
    Added 30s timeouts:
    - `mail.smtp.connectiontimeout`
    - `mail.smtp.timeout`
    - `mail.smtp.writetimeout`

--- platform-core-module/src/main/java/com/softicar/platform/core/module/email/buffer/BufferedEmailSenderSessionManager.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/email/buffer/BufferedEmailSenderSessionManager.java
@@ -4,6 +4,7 @@ import com.softicar.platform.core.module.server.AGServer;
 import jakarta.mail.Authenticator;
 import jakarta.mail.PasswordAuthentication;
 import jakarta.mail.Session;
+import java.time.Duration;
 import java.util.Map;
 import java.util.Properties;
 import java.util.TreeMap;
@@ -27,18 +28,30 @@ public class BufferedEmailSenderSessionManager {
 		return Session.getInstance(new SessionProperties(server), new PasswordAuthenticator(server));
 	}
 
+	/**
+	 * See <a href=
+	 * "https://javaee.github.io/javamail/docs/api/index.html?com/sun/mail/smtp/package-summary.html">com.sun.mail.smtp
+	 * properties documentation</a>.
+	 */
 	private static class SessionProperties extends Properties {
 
 		public SessionProperties(AGServer server) {
 
-			put("mail.transport.protocol", "smtp");
-			put("mail.smtp.starttls.enable", "true");
-//			put("mail.smtp.starttls.required", "true");
+			// basics
 			put("mail.smtp.host", server.getAddress());
 			put("mail.smtp.port", server.getPort());
 			put("mail.smtp.user", server.getUsername());
+			put("mail.transport.protocol", "smtp");
+
+			// timeouts
+			put("mail.smtp.connectiontimeout", Duration.ofSeconds(30).toMillis());
+			put("mail.smtp.timeout", Duration.ofSeconds(30).toMillis());
+			put("mail.smtp.writetimeout", Duration.ofSeconds(30).toMillis());
+
+			// security
 			put("mail.smtp.auth", "true");
 			put("mail.smtp.ssl.trust", "*");
+			put("mail.smtp.starttls.enable", "true");
 		}
 	}
 
