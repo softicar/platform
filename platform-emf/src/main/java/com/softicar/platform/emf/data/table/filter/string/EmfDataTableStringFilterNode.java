f0babb7c0b56675ea720b92e04b0bfe46a41d420
Viktor Nikolaus <viktor.nikolaus@forspace-solutions.com>
2023-05-04 15:16:51 +0200

    Further renaming

--- platform-emf/src/main/java/com/softicar/platform/emf/data/table/filter/string/EmfDataTableStringFilterNode.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/data/table/filter/string/EmfDataTableStringFilterNode.java
@@ -36,7 +36,7 @@ public class EmfDataTableStringFilterNode<R> extends AbstractEmfDataTableMultiTy
 				return new ContainsWordsFilter<>(column.getDataColumn(), filterText, resetter);
 			case CONTAINS_TEXT:
 				return new ContainsTextFilter<>(column.getDataColumn(), filterText, resetter);
-			case CONTAINS_NOT_TEXT:
+			case DOES_NOT_CONTAIN_TEXT:
 				return new DoesNotContainTextFilter<>(column.getDataColumn(), filterText, resetter);
 			case EQUALS_TEXT:
 				return new EqualsTextFilter<>(column.getDataColumn(), filterText, resetter);
