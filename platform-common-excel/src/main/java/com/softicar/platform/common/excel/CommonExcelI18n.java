52aa8aaebf2a011f518208ab9cd2228a33c90c08
Melisa09 <59706957+Melisa09@users.noreply.github.com>
2023-05-24 12:55:46 +0200

    PLAT-1375-MH1 I18n translation (#812)

--- platform-common-excel/src/main/java/com/softicar/platform/common/excel/CommonExcelI18n.java
+++ platform-common-excel/src/main/java/com/softicar/platform/common/excel/CommonExcelI18n.java
@@ -5,5 +5,8 @@ import com.softicar.platform.common.core.i18n.I18n1;
 public interface CommonExcelI18n {
 
 	I18n1 FAILED_TO_PARSE_EXCEL_FILE_ARG1 = new I18n1("Failed to parse Excel file: %s")//
-		.de("Einlesen der Excel-Datei fehlgeschlagen: %s");
+		.de("Einlesen der Excel-Datei fehlgeschlagen: %s")
+		.bs("Nije uspjelo čitanje Excel fajla: %s")
+		.sr("Nije uspelo čitanje Excel fajla: %s")
+		.hr("Nije uspjelo čitanje Excel fajla: %s");
 }
--- platform-common/src/main/java/com/softicar/platform/common/core/CommonCoreI18n.java
+++ platform-common/src/main/java/com/softicar/platform/common/core/CommonCoreI18n.java
@@ -12,9 +12,9 @@ public interface CommonCoreI18n {
 		.hr("Decimalni razdjelnik");
 	I18n0 DIGIT_GROUP_SEPARATOR = new I18n0("Digit Group Separator")//
 		.de("Zifferngruppentrennzeichen")
-		.bs("Simbol grupisanja cifara")
-		.sr("Simbol grupisanja cifara")
-		.hr("Simbol grupiranja znamenki");
+		.bs("Separator grupisanja cifara")
+		.sr("Separator grupisanja cifara")
+		.hr("Razdjelnik grupiranja znamenki");
 	I18n0 DIGIT_GROUP_TOO_LONG = new I18n0("Digit group too long.")//
 		.de("Zifferngruppe zu lang.")
 		.bs("Grupa cifara je predugačka.")
@@ -29,7 +29,7 @@ public interface CommonCoreI18n {
 		.de("Unzulässige Zeichen für Dezimalzahl: %s")
 		.bs("Nedozvoljeni znakovi za decimalni broj: %s")
 		.sr("Nedozvoljeni znakovi za decimalni broj: %s")
-		.hr("Nedozvoljene znakovi za decimalni broj: %s");
+		.hr("Nedozvoljeni karakteri za decimalni broj: %s");
 	I18n0 INVALID_DATE_FORMAT = new I18n0("Invalid date format.")//
 		.de("Ungültiges Datumsformat.")
 		.bs("Nevažeći format datuma.")
@@ -42,40 +42,40 @@ public interface CommonCoreI18n {
 		.hr("Povratak na prijavu");
 	I18n0 THE_DECIMAL_PART_MUST_NOT_CONTAIN_DIGIT_GROUP_SEPARATORS = new I18n0("The decimal part must not contain digit group separators.")//
 		.de("Der Dezimalteil darf keine Zifferngruppentrennzeichen enthalten.")
-		.bs("Decimalni dio ne smije sadržavati simbol grupisanja cifara.")
-		.sr("Decimalni dio ne sme sadržavati simbol grupisanja cifara.")
-		.hr("Decimalni dio ne smije sadržavati simbol grupisanja cifara.");
+		.bs("Decimalni dio ne smije sadržavati separator grupisanja cifara.")
+		.sr("Decimalni dio ne sme sadržavati separator grupisanja cifara.")
+		.hr("Decimalni dio ne smije sadržavati razdjelnik grupisanja znamenki.");
 	I18n0 THE_DECIMAL_SEPARATOR_MAY_NOT_BE_EMPTY = new I18n0("The decimal separator may not be empty.")//
 		.de("Das Dezimaltrennzeichen darf nicht leer sein.")
 		.bs("Decimalni separator ne smije biti prazan.")
 		.sr("Decimalni separator ne sme biti prazan.")
-		.hr("Decimalni separator ne smije biti prazan.");
+		.hr("Decimalni razdjelnik ne smije biti prazan.");
 	I18n0 THE_DECIMAL_SEPARATOR_MUST_BE_DIFFERENT_FROM_THE_DIGIT_GROUP_SEPARATOR =
 			new I18n0("The decimal separator must be different from the digit group separator.")//
 				.de("Das Dezimaltrennzeichen muss sich vom Zifferngruppentrennzeichen unterscheiden.")
-				.bs("Decimalni separator mora biti različit od simbola grupisanja cifara.")
-				.sr("Decimalni separator mora biti različit od simbola grupisanja cifara.")
-				.hr("Decimalni separator mora biti različit od simbola grupisanja cifara.");
+				.bs("Decimalni separator mora biti različit od separatora grupisanja cifara.")
+				.sr("Decimalni separator mora biti različit od separatora grupisanja cifara.")
+				.hr("Decimalni razdjelnik mora biti različit od razdjelnika grupisanja znamenki.");
 	I18n1 THE_DECIMAL_SEPARATOR_MUST_NOT_CONTAIN_ANY_OF_THE_FOLLOWING_CHARACTERS_ARG1 =
 			new I18n1("The decimal separator must not contain any of the following characters: %s")//
 				.de("Das Dezimaltrennzeichen darf keines der folgenden Zeichen enthalten: %s")
-				.bs("Decimalni separator ne smije sadržavati nijedan of sljedećih karaktera: %s")
-				.sr("Decimalni separator ne smije sadržavati nijedan of sledećih karaktera: %s")
-				.hr("Decimalni separator ne smije sadržavati nijedan of sljedećih karaktera: %s");
+				.bs("Decimalni separator ne smije sadržavati nijedan of sljedećih znakova: %s")
+				.sr("Decimalni separator ne sme sadržavati nijedan of sledećih znakova: %s")
+				.hr("Decimalni razdjelnik ne smije sadržavati nijedan of sljedećih karaktera: %s");
 	I18n0 THE_DECIMAL_SEPARATOR_MUST_NOT_CONTAIN_DIGITS = new I18n0("The decimal separator must not contain digits.")//
 		.de("Das Dezimaltrennzeichen darf keine Ziffern beinhalten.")
 		.bs("Decimalni separator ne smije sadržavati cifre.")
-		.sr("Decimalni separator ne smije sadržavati cifre.")
-		.hr("Decimalni separator ne smije sadržavati cifre.");
+		.sr("Decimalni separator ne sme sadržavati cifre.")
+		.hr("Decimalni razdjelnik ne smije sadržavati znamenke.");
 	I18n1 THE_DIGIT_GROUP_SEPARATOR_MUST_NOT_CONTAIN_ANY_OF_THE_FOLLOWING_CHARACTERS_ARG1 =
 			new I18n1("The digit group separator must not contain any of the following characters: %s")//
 				.de("Das Zifferngruppentrennzeichen darf keines der folgenden Zeichen enthalten: %s")
-				.bs("Simbol grupisanja cifara ne smije sadržavati nijedan of sljedećih karaktera: %s")
-				.sr("Simbol grupisanja cifara ne smije sadržavati nijedan of sledećih karaktera: %s")
-				.hr("Simbol grupiranja cifara ne smije sadržavati nijedan of sljedećih karaktera: %s");
+				.bs("Separator grupisanja cifara ne smije sadržavati nijedan of sljedećih znakova: %s")
+				.sr("Separator grupisanja cifara ne sme sadržavati nijedan of sledećih znakova: %s")
+				.hr("Razdjelnik grupiranja znamenki ne smije sadržavati nijedan of sljedećih karaktera: %s");
 	I18n0 THE_DIGIT_GROUP_SEPARATOR_MUST_NOT_CONTAIN_DIGITS = new I18n0("The digit group separator must not contain digits.")//
 		.de("Das Zifferngruppentrennzeichen darf keine Ziffern beinhalten.")
-		.bs("Simbol grupisanja cifara ne smije sadržavati cifre.")
-		.sr("Simbol grupisanja cifara ne smije sadržavati cifre.")
-		.hr("Simbol grupiranja cifara ne smije sadržavati znamenke.");
+		.bs("Separator grupisanja cifara ne smije sadržavati cifre.")
+		.sr("Separator grupisanja cifara ne sme sadržavati cifre.")
+		.hr("Razdjelnik grupiranja znamenki ne smije sadržavati znamenke.");
 }
