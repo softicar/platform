6735476f8981772b418be0403922a742b7cad39e
Melisa09 <59706957+Melisa09@users.noreply.github.com>
2023-05-30 14:32:17 +0200

    Translated CommonDate, CommonString, Demo and Workflow I18n's. (#823)
    Co-authored-by: muhamed.saric <muhamed.saric@forspace-solutions.com>

--- platform-common/src/main/java/com/softicar/platform/common/string/CommonStringI18n.java
+++ platform-common/src/main/java/com/softicar/platform/common/string/CommonStringI18n.java
@@ -5,5 +5,8 @@ import com.softicar.platform.common.core.i18n.I18n2;
 public interface CommonStringI18n {
 
 	I18n2 CSV_SYNTAX_ERROR_IN_LINE_ARG1_AT_CHARACTER_ARG2 = new I18n2("CSV syntax error in line %s at character %s")//
-		.de("CSV-Syntaxfehler in Zeile %s bei Zeichen %s");
+		.de("CSV-Syntaxfehler in Zeile %s bei Zeichen %s")
+		.bs("Sintaksna greška na liniji %s na karakteru %s")
+		.hr("Sintaksna greška na liniji %s na karakteru %s")
+		.sr("Sintaksna greška na liniji %s na karakteru %s");
 }
