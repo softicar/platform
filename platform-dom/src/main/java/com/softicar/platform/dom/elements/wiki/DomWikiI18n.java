58ac372855dff11a143fdf52bcba37244920c50a
Melisa09 <59706957+Melisa09@users.noreply.github.com>
2023-06-02 10:27:33 +0200

    PLAT-1375-MH4 Translated documents (DomWikiI18n and EmfDataTabelI18n) to Bosnian, Croatian and Serbian (#829)
    * PLAT-1375-MH4 Translated documents (DomWikiI182 and EmfDataTabelI18n) to
    Bosnian, Croatian and Serbian
    
    * Fixed typo for EmfDataTableI18n.java
    
    * Fixed typo for  EmfDataTableI18n.NONE: Expected name: NOTHING

--- platform-dom/src/main/java/com/softicar/platform/dom/elements/wiki/DomWikiI18n.java
+++ platform-dom/src/main/java/com/softicar/platform/dom/elements/wiki/DomWikiI18n.java
@@ -8,77 +8,182 @@ import com.softicar.platform.common.core.i18n.I18n3;
 public interface DomWikiI18n {
 
 	I18n0 BASIC_TEXT_FORMATTING = new I18n0("Basic Text Formatting")//
-		.de("Grundlegende Textformatierung");
+		.de("Grundlegende Textformatierung")
+		.bs("Osnovno formatiranje teksta")
+		.sr("Osnovno formatiranje teksta")
+		.hr("Osnovno formatiranje teksta");
 	I18n0 BOXES = new I18n0("Boxes")//
-		.de("Boxen");
+		.de("Boxen")
+		.bs("Kutije")
+		.sr("Kutije")
+		.hr("Kutije");
 	I18n0 CELL = new I18n0("Cell")//
-		.de("Zelle");
+		.de("Zelle")
+		.bs("Ćelija")
+		.sr("Ćelija")
+		.hr("Ćelija");
 	I18n0 CURRENTLY_RESOURCES_ARE_ONLY_SUPPORTED_FOR_WIKI_ELEMENTS_THAT_ARE_DEFINED_IN_JAVA =
 			new I18n0("Currently, resources are **only** supported for wiki elements that are defined in **Java**.")//
-				.de("Ressourcen für Wiki Elemente werden momentan **nur** unterstützt, wenn diese in **Java** definiert sind.");
+				.de("Ressourcen für Wiki Elemente werden momentan **nur** unterstützt, wenn diese in **Java** definiert sind.")
+				.bs("Trenutno su resursi podržani **samo** za wiki elemente koji su definirani u **Javi**.")
+				.sr("Trenutno su resursi podržani **samo** za wiki elemente koji su definirani u **Javi**.")
+				.hr("Trenutno su resursi podržani **samo** za wiki elemente koji su definirani u **Javi**.");
 	I18n3 DIFFERENT_TYPES_OF_BOXES_ARG1_ARG2_AND_ARG3 = new I18n3("Different types of boxes: %s, %s and %s.")//
-		.de("Unterschiedliche Arten von Boxen: %s, %s und %s.");
+		.de("Unterschiedliche Arten von Boxen: %s, %s und %s.")
+		.bs("Različite vrste kutija: %s, %s i %s.")
+		.sr("Različite vrste kutija: %s, %s i %s.")
+		.hr("Različite vrste kutija: %s, %s i %s.");
 	I18n0 HEADER = new I18n0("Header")//
-		.de("Kopfzeile");
+		.de("Kopfzeile")
+		.bs("Zaglavlje")
+		.sr("Zaglavlje")
+		.hr("Zaglavlje");
 	I18n0 HEADLINE = new I18n0("Headline")//
-		.de("Überschrift");
+		.de("Überschrift")
+		.bs("Naslov")
+		.sr("Naslov")
+		.hr("Naslov");
 	I18n0 HEADLINES = new I18n0("Headlines")//
-		.de("Überschriften");
+		.de("Überschriften")
+		.bs("Naslovi")
+		.sr("Naslovi")
+		.hr("Naslovi");
 	I18n1 HEADLINES_ARE_CREATED_BY_USING_TWO_OR_MORE_EQUALS_SIGNS_ARG1 = new I18n1("Headlines are created by using two or more **equals signs** %s.")//
-		.de("Überschriften werden erstellt, indem man zwei oder mehrere **Gleichheitszeichen** %s benutzt.");
+		.de("Überschriften werden erstellt, indem man zwei oder mehrere **Gleichheitszeichen** %s benutzt.")
+		.bs("Naslovi se kreiraju korištenjem dva ili više **znakova jednakosti** %s.")
+		.sr("Naslovi se kreiraju korištenjem dva ili više **znakova jednakosti** %s.")
+		.hr("Naslovi se kreiraju korištenjem dva ili više **znakova jednakosti** %s.");
 	I18n0 IF_YOU_WANT_TO_FORCE_A_NEWLINE_WITHOUT_A_PARAGRAPH_YOU_CAN_USE_TWO_BACKSLASHES_FOLLOWED_BY_A_WHITESPACE_OR_THE_END_OF_LINE = new I18n0(
 		"If you want to **force a newline** without a paragraph, you can use two backslashes followed by a whitespace or the end of line.")//
 			.de(
-				"Wenn sie einen **Zeilenumbruch** ohne einen neuen Paragraphen **erzwingen** wollen, können sie zwei Backslashes gefolgt von einer Leerstelle oder dem Ende der Zeile verwenden.");
+				"Wenn sie einen **Zeilenumbruch** ohne einen neuen Paragraphen **erzwingen** wollen, können sie zwei Backslashes gefolgt von einer Leerstelle oder dem Ende der Zeile verwenden.")
+			.bs("Ako želite **preći u novi red** bez paragrafa, možete koristiti dvije kose crte praćene razmakom ili krajem reda.")
+			.sr("Ako želite **preći u novi red** bez paragrafa, možete koristiti dvije kose crte praćene razmakom ili krajem reda.")
+			.hr("Ako želite **preći u novi red** bez paragrafa, možete koristiti dvije kose crte praćene razmakom ili krajem reda.");
 	I18n0 ITALIC_UNDERLINE_AND_MONOSPACED_TEXT = new I18n0("//italic//, __underline__ and ''monospaced'' text.")//
-		.de("//kursiver//, __unterstrichener__ und ''dicktengleicher'' Text.");
+		.de("//kursiver//, __unterstrichener__ und ''dicktengleicher'' Text.")
+		.bs("//italic//, __underline__ i ''monospaced'' tekst.")
+		.sr("//italic//, __underline__ i ''monospaced'' tekst.")
+		.hr("//italic//, __underline__ i ''monospaced'' tekst.");
 	I18n0 LINKS = new I18n0("Links")//
-		.de("Links");
+		.de("Links")
+		.bs("Linkovi")
+		.sr("Linkovi")
+		.hr("Linkovi");
 	I18n0 LISTS = new I18n0("Lists")//
-		.de("Listen");
+		.de("Listen")
+		.bs("Liste")
+		.sr("Liste")
+		.hr("Liste");
 	I18n0 OF_COURSE_YOU_CAN_COMBINE_ALL_THESE = new I18n0("Of course you can **__//''combine''//__** all these.")//
-		.de("Sie können natürlich auch alle **__//''kombinieren''//__**.");
+		.de("Sie können natürlich auch alle **__//''kombinieren''//__**.")
+		.bs("Naravno da možete **__//''kombinirati''//__** sve ove.")
+		.sr("Naravno da možete **__//''kombinirati''//__** sve ove.")
+		.hr("Naravno da možete **__//''kombinirati''//__** sve ove.");
 	I18n0 ORDERED_LIST_ITEM = new I18n0("ordered list item")//
-		.de("Element einer geordneten Liste");
+		.de("Element einer geordneten Liste")
+		.bs("stavka uređene liste")
+		.sr("stavka uređene liste")
+		.hr("stavka uređene liste");
 	I18n0 PARAGRAPHS_ARE_CREATED_FROM_BLANK_LINES = new I18n0("**Paragraphs** are created from blank lines.")//
-		.de("**Paragraphen** werden durch leere Zeilen erstellt.");
+		.de("**Paragraphen** werden durch leere Zeilen erstellt.")
+		.bs("**Paragrafi** se stvaraju iz praznih linija.")
+		.sr("**Paragrafi** se stvaraju iz praznih linija.")
+		.hr("**Paragrafi** se stvaraju iz praznih linija.");
 	I18n0 RESOURCES = new I18n0("Resources")//
-		.de("Ressourcen");
+		.de("Ressourcen")
+		.bs("Resursi")
+		.sr("Resursi")
+		.hr("Resursi");
 	I18n0 SOFTICAR_WIKI_SYNTAX = new I18n0("SoftiCAR Wiki Syntax")//
-		.de("SoftiCAR Wiki Syntax");
+		.de("SoftiCAR Wiki Syntax")
+		.bs("SoftiCAR Wiki sintaksa")
+		.sr("SoftiCAR Wiki sintaksa")
+		.hr("SoftiCAR Wiki sintaksa");
 	I18n0 TABLES = new I18n0("Tables")//
-		.de("Tabellen");
+		.de("Tabellen")
+		.bs("Tabele")
+		.sr("Tabele")
+		.hr("Tabele");
 	I18n1 THE_SOFTICAR_WIKI_SYNTAX_IS_VERY_SIMILAR_TO_THE_ARG1_DOKUWIKI_SYNTAX =
 			new I18n1("The SoftiCAR Wiki syntax is very similar to the [[%s|Dokuwiki syntax]].")//
-				.de("Die Softicar Wiki Syntax ist sehr ähnlich zu der [[%s|Dokuwiki Syntax]].");
+				.de("Die Softicar Wiki Syntax ist sehr ähnlich zu der [[%s|Dokuwiki Syntax]].")
+				.bs("SoftiCAR Wiki sintaksa vrlo je slična [[%s|Dokuwiki sintaksi]].")
+				.sr("SoftiCAR Wiki sintaksa vrlo je slična [[%s|Dokuwiki sintaksi]].")
+				.hr("SoftiCAR Wiki sintaksa vrlo je slična [[%s|Dokuwiki sintaksi]].");
 	I18n0 THE_SOFTICAR_WIKI_SYNTAX_SUPPORTS_BOLD = new I18n0("The SoftiCAR Wiki syntax supports **bold**,")//
-		.de("Die SoftiCAR Wiki Syntax unterstüzt **fettgedruckt**,");
+		.de("Die SoftiCAR Wiki Syntax unterstüzt **fettgedruckt**,")
+		.bs("SoftiCAR Wiki sintaksa podržava **bold**,")
+		.sr("SoftiCAR Wiki sintaksa podržava **bold**,")
+		.hr("SoftiCAR Wiki sintaksa podržava **bold**,");
 	I18n0 THERE_ARE_FIVE_DIFFERENT_LEVELS_OF_HEADLINES = new I18n0("There are **five** different levels of headlines.")//
-		.de("Es gibt **fünf** verschiedene Stufen von Überschriften.");
+		.de("Es gibt **fünf** verschiedene Stufen von Überschriften.")
+		.bs("Postoji **pet** različitih nivoa naslova.")
+		.sr("Postoji **pet** različitih nivoa naslova.")
+		.hr("Postoji **pet** različitih nivoa naslova.");
 	I18n0 THIS_IS_A_WARNING_BOX = new I18n0("This is a warning box.")//
-		.de("Das ist eine Warnungs-Box.");
+		.de("Das ist eine Warnungs-Box.")
+		.bs("Ovo je okvir upozorenja.")
+		.sr("Ovo je okvir upozorenja.")
+		.hr("Ovo je okvir upozorenja.");
 	I18n0 THIS_IS_AN_ERROR_BOX = new I18n0("This is an error box.")//
-		.de("Das ist eine Fehler-Box.");
+		.de("Das ist eine Fehler-Box.")
+		.bs("Ovo je okvir sa greškom.")
+		.sr("Ovo je okvir sa greškom.")
+		.hr("Ovo je okvir sa greškom.");
 	I18n0 THIS_IS_AN_INFO_BOX_WITH_A_LIST_FOO_BAR = new I18n0("This is an info box with a list:  * foo  * bar")//
-		.de("Das ist eine Info-Box mit einer Liste:  * foo  * bar");
+		.de("Das ist eine Info-Box mit einer Liste:  * foo  * bar")
+		.bs("Ovo je informacijski okvir sa listom: * foo * bar")
+		.sr("Ovo je informacijski okvir sa listom: * foo * bar")
+		.hr("Ovo je informacijski okvir sa listom: * foo * bar");
 	I18n0 UNORDERED_LIST_ITEM = new I18n0("unordered list item")//
-		.de("Element einer ungeordneten Liste");
+		.de("Element einer ungeordneten Liste")
+		.bs("stavka neuređene liste")
+		.sr("stavka neuređene liste")
+		.hr("stavka neuređene liste");
 	I18n1 USE_ARG1_FOR_TABLE_BODY_CELLS = new I18n1("use %s for table body cells")//
-		.de("%s für Zellen im Tabellenkörper nutzen");
+		.de("%s für Zellen im Tabellenkörper nutzen")
+		.bs("koristite %s za ćelije tijela tabele")
+		.sr("koristite %s za ćelije tijela tabele")
+		.hr("koristite %s za ćelije tijela tabele");
 	I18n1 USE_ARG1_FOR_TABLE_HEADER_CELLS = new I18n1("use %s for table header cells")//
-		.de("%s für Zellen in der Tabellenüberschrift nutzen");
+		.de("%s für Zellen in der Tabellenüberschrift nutzen")
+		.bs("koristite %s za ćelije zaglavlja tabele")
+		.sr("koristite %s za ćelije zaglavlja tabele")
+		.hr("koristite %s za ćelije zaglavlja tabele");
 	I18n0 WIKI_SYNTAX = new I18n0("Wiki Syntax")//
-		.de("Wiki Syntax");
+		.de("Wiki Syntax")
+		.bs("Wiki sintaksa")
+		.sr("Wiki sintaksa")
+		.hr("Wiki sintaksa");
 	I18n0 WITH_LABEL = new I18n0("With Label")//
-		.de("Mit Beschriftung");
+		.de("Mit Beschriftung")
+		.bs("S oznakom")
+		.sr("S oznakom")
+		.hr("S oznakom");
 	I18n1 YOU_CAN_ALSO_ADD_RESOURCES_LIKE_IMAGES_ARG1 = new I18n1("You can also add resources like images %s.")//
-		.de("Sie können auch Ressourcen wie Bilder hinzufügen %s.");
+		.de("Sie können auch Ressourcen wie Bilder hinzufügen %s.")
+		.bs("Također možete dodati resurse kao što su slike %s.")
+		.sr("Također možete dodati resurse kao što su slike %s.")
+		.hr("Također možete dodati resurse kao što su slike %s.");
 	I18n1 YOU_CAN_CREATE_ORDERED_LISTS_BY_STARTING_A_LINE_WITH_ARG1 = new I18n1("You can create **ordered** lists by starting a line with %s.")//
-		.de("Sie können **geordnete** Listen erstellen indem sie die Zeile mit %s beginnen.");
+		.de("Sie können **geordnete** Listen erstellen indem sie die Zeile mit %s beginnen.")
+		.bs("Možete kreirati **uređene** liste započinjanjem reda s %s.")
+		.sr("Možete kreirati **uređene** liste započinjanjem reda s %s.")
+		.hr("Možete kreirati **uređene** liste započinjanjem reda s %s.");
 	I18n1 YOU_CAN_CREATE_UNORDERED_LISTS_BY_STARTING_A_LINE_WITH_ARG1 = new I18n1("You can create **unordered** lists by starting a line with %s.")//
-		.de("Sie können **ungeordnete** Listen erstellen indem sie die Zeile mit %s beginnen.");
+		.de("Sie können **ungeordnete** Listen erstellen indem sie die Zeile mit %s beginnen.")
+		.bs("Možete kreirati **neuređene** liste započinjanjem reda s %s.")
+		.sr("Možete kreirati **neuređene** liste započinjanjem reda s %s.")
+		.hr("Možete kreirati **neuređene** liste započinjanjem reda s %s.");
 	I18n0 YOU_CAN_DEFINE_EXTERNAL_LINKS_WITH_AND_WITHOUT_LABEL = new I18n0("You can define external links with and without label.")//
-		.de("Sie können externe Links mit oder ohne Beschriftung erstellen.");
+		.de("Sie können externe Links mit oder ohne Beschriftung erstellen.")
+		.bs("Vanjske linkove možete definirati sa i bez oznake.")
+		.sr("Vanjske linkove možete definirati sa i bez oznake.")
+		.hr("Vanjske linkove možete definirati sa i bez oznake.");
 	I18n2 YOU_CAN_DEFINE_TABLES_BY_USING_ARG1_AND_ARG2 = new I18n2("You can define **tables** by using %s and %s.")//
-		.de("Sie können **Tabellen** erstellen indem sie %s und %s nutzen.");
+		.de("Sie können **Tabellen** erstellen indem sie %s und %s nutzen.")
+		.bs("Možete definirati **tabele** koristeći %s i %s.")
+		.sr("Možete definirati **tabele** koristeći %s i %s.")
+		.hr("Možete definirati **tabele** koristeći %s i %s.");
 }
--- platform-emf/src/main/java/com/softicar/platform/emf/data/table/EmfDataTableI18n.java
+++ platform-emf/src/main/java/com/softicar/platform/emf/data/table/EmfDataTableI18n.java
@@ -7,91 +7,223 @@ import com.softicar.platform.common.core.i18n.I18n2;
 public interface EmfDataTableI18n {
 
 	I18n0 _1_ROW_SELECTED = new I18n0("1 row selected")//
-		.de("1 Zeile ausgewählt");
+		.de("1 Zeile ausgewählt")
+		.bs("1 red je odabran")
+		.sr("1 red je odabran")
+		.hr("1 red je odabran");
 	I18n0 _1_ROW_SELECTED_ON_ANOTHER_PAGE = new I18n0("1 row selected on another page")//
-		.de("1 Zeile auf einer anderen Seite ausgewählt");
+		.de("1 Zeile auf einer anderen Seite ausgewählt")
+		.bs("1 red odabran na drugoj stranici")
+		.sr("1 red odabran na drugoj stranici")
+		.hr("1 red odabran na drugoj stranici");
 	I18n0 ADD_FILTER = new I18n0("Add Filter")//
-		.de("Filter hinzufügen");
+		.de("Filter hinzufügen")
+		.bs("Dodajte filter")
+		.sr("Dodajte filter")
+		.hr("Dodajte filter");
 	I18n0 ALL = new I18n0("All")//
-		.de("Alle");
+		.de("Alle")
+		.bs("Sve")
+		.sr("Sve")
+		.hr("Sve");
 	I18n0 APPLY = new I18n0("Apply")//
-		.de("Übernehmen");
+		.de("Übernehmen")
+		.bs("Primijenite")
+		.sr("Primijenite")
+		.hr("Primijenite");
 	I18n2 ARG1_OF_ARG2_ROWS_SELECTED_ON_OTHER_PAGES = new I18n2("%s of %s rows selected on other pages.")//
-		.de("%s von %s Zeilen auf anderen Seiten ausgewählt.");
+		.de("%s von %s Zeilen auf anderen Seiten ausgewählt.")
+		.bs("%s od %s kolona odabranih na drugim stranicama.")
+		.sr("%s od %s kolona odabranih na drugim stranicama.")
+		.hr("%s od %s kolona odabranih na drugim stranicama.");
 	I18n1 ARG1_ROWS_SELECTED = new I18n1("%s rows selected")//
-		.de("%s Zeilen ausgewählt");
+		.de("%s Zeilen ausgewählt")
+		.bs("%s kolona je odabrano")
+		.sr("%s kolona je odabrano")
+		.hr("%s kolona je odabrano");
 	I18n1 ARG1_TOTAL = new I18n1("%s total")//
-		.de("%s insgesamt");
+		.de("%s insgesamt")
+		.bs("%s ukupno")
+		.sr("%s ukupno")
+		.hr("%s ukupno");
 	I18n0 CLICK_TO_FILTER_IN_THIS_COLUMN = new I18n0("Click to filter in this column")//
-		.de("Klicken, um in dieser Spalte zu filtern");
+		.de("Klicken, um in dieser Spalte zu filtern")
+		.bs("Kliknite za filtriranje u ovoj koloni")
+		.sr("Kliknite za filtriranje u ovoj koloni")
+		.hr("Kliknite za filtriranje u ovoj koloni");
 	I18n0 CLICK_TO_TOGGLE_OPERATION = new I18n0("Click to toggle operation.")//
-		.de("Klicken zum umkehren");
+		.de("Klicken zum umkehren")
+		.bs("Kliknite za promjenu radnje.")
+		.sr("Kliknite za promjenu radnje.")
+		.hr("Kliknite za promjenu radnje.");
 	I18n0 COLUMN = new I18n0("Column")//
-		.de("Spalte");
+		.de("Spalte")
+		.bs("Kolona")
+		.sr("Kolona")
+		.hr("Kolona");
 	I18n0 CONTAINS = new I18n0("contains")//
-		.de("enthält");
+		.de("enthält")
+		.bs("sadrži")
+		.sr("sadrži")
+		.hr("sadrži");
 	I18n0 CONTAINS_TEXT = new I18n0("contains text")//
-		.de("enthält Text");
+		.de("enthält Text")
+		.bs("sadrži tekst")
+		.sr("sadrži tekst")
+		.hr("sadrži tekst");
 	I18n0 CONTAINS_WORDS = new I18n0("contains words")//
-		.de("enthält Wörter");
+		.de("enthält Wörter")
+		.bs("sadržava riječi")
+		.sr("sadržava riječi")
+		.hr("sadržava riječi");
 	I18n0 DOES_NOT_CONTAIN = new I18n0("does not contain")//
-		.de("enthält nicht");
+		.de("enthält nicht")
+		.bs("ne sadržava")
+		.sr("ne sadržava")
+		.hr("ne sadržava");
 	I18n0 DOES_NOT_CONTAIN_TEXT = new I18n0("does not contain text")//
-		.de("enthält nicht Text");
+		.de("enthält nicht Text")
+		.bs("ne sadrži tekst")
+		.sr("ne sadrži tekst")
+		.hr("ne sadrži tekst");
 	I18n0 EQUALS_TEXT = new I18n0("equals text")//
-		.de("entspricht Text");
+		.de("entspricht Text")
+		.bs("odgovara tekstu")
+		.sr("odgovara tekstu")
+		.hr("odgovara tekstu");
 	I18n0 FILTER = new I18n0("Filter")//
-		.de("Filtern");
+		.de("Filtern")
+		.bs("Filter")
+		.sr("Filter")
+		.hr("Filter");
 	I18n0 FILTER_BY_COLUMN = new I18n0("Filter by Column")//
-		.de("Nach Spalte filtern");
+		.de("Nach Spalte filtern")
+		.bs("Filtrirajte po kolonama")
+		.sr("Filtrirajte po kolonama")
+		.hr("Filtrirajte po kolonama");
 	I18n0 HOLD_DOWN_CONTROL_KEY_FOR_MULTI_COLUMN_SORTING = new I18n0("Hold down control key for multi-column sorting.")//
-		.de("Steuerung-Taste gedrückt halten für Sortierung über mehrere Spalten.");
+		.de("Steuerung-Taste gedrückt halten für Sortierung über mehrere Spalten.")
+		.bs("Držite pritisnutu kontrolnu tipku za sortiranje više kolona.")
+		.sr("Držite pritisnutu kontrolnu tipku za sortiranje više kolona.")
+		.hr("Držite pritisnutu kontrolnu tipku za sortiranje više kolona.");
 	I18n0 INVERT = new I18n0("Invert")//
-		.de("Umkehren");
+		.de("Umkehren")
+		.bs("Okrenuti se nazad")
+		.sr("Okrenuti se nazad")
+		.hr("Okrenuti se natrag");
 	I18n0 IS = new I18n0("is")//
-		.de("ist");
+		.de("ist")
+		.bs("je")
+		.sr("je")
+		.hr("je");
 	I18n0 IS_EMPTY = new I18n0("is empty")//
-		.de("ist leer");
+		.de("ist leer")
+		.bs("prazno je")
+		.sr("prazno je")
+		.hr("prazno je");
 	I18n0 IS_NOT = new I18n0("is not")//
-		.de("ist nicht");
+		.de("ist nicht")
+		.bs("nije")
+		.sr("nije")
+		.hr("nije");
 	I18n0 IS_NOT_EMPTY = new I18n0("is not empty")//
-		.de("ist nicht leer");
+		.de("ist nicht leer")
+		.bs("nije prazno")
+		.sr("nije prazno")
+		.hr("nije prazno");
 	I18n0 MATCHES_REGULAR_EXPRESSION = new I18n0("matches regular expression")//
-		.de("entspricht regulärem Ausdruck");
+		.de("entspricht regulärem Ausdruck")
+		.bs("podudara se s regularnim izrazom")
+		.sr("podudara se s regularnim izrazom")
+		.hr("podudara se s regularnim izrazom");
 	I18n0 MOVE_DOWN = new I18n0("move down")//
-		.de("Verschiebe nach unten");
+		.de("Verschiebe nach unten")
+		.bs("pomjerite se prema dolje")
+		.sr("pomerite se prema dolje")
+		.hr("pomjerite se prema dolje");
 	I18n0 MOVE_UP = new I18n0("move up")//
-		.de("Verschiebe nach oben");
+		.de("Verschiebe nach oben")
+		.bs("pomjerite se prema gore")
+		.sr("pomerite se prema gore")
+		.hr("pomjerite se prema gore");
 	I18n0 NO = new I18n0("No")//
-		.de("Nein");
+		.de("Nein")
+		.bs("Ne")
+		.sr("Ne")
+		.hr("Ne");
 	I18n0 NO_ENTRIES_FOUND = new I18n0("no entries found")//
-		.de("keine Einträge gefunden");
+		.de("keine Einträge gefunden")
+		.bs("nema pronađenih unosa")
+		.sr("nema pronađenih unosa")
+		.hr("nema pronađenih unosa");
 	I18n1 NO_SORTING_OF_COLUMN_ARG1 = new I18n1("No sorting of column '%s'.")//
-		.de("Keine Sortierung in Spalte '%s'.");
+		.de("Keine Sortierung in Spalte '%s'.")
+		.bs("Nema sortiranja kolona '%s'.")
+		.sr("Nema sortiranja kolona '%s'.")
+		.hr("Nema sortiranja kolona '%s'.");
 	I18n0 NONE = new I18n0("None")//
-		.de("Nichts");
+		.de("Nichts")
+		.bs("Ništa")
+		.sr("Ništa")
+		.hr("Ništa");
 	I18n0 NONE_ALL_PAGES = new I18n0("None (All Pages)")//
-		.de("Nichts (alle Seiten)");
+		.de("Nichts (alle Seiten)")
+		.bs("Ništa (sve stranice)")
+		.sr("Ništa (sve stranice)")
+		.hr("Ništa (sve stranice)");
 	I18n0 ORDERING_PRIORITY = new I18n0("Ordering / Priority")//
-		.de("Sortierung / Priorität");
+		.de("Sortierung / Priorität")
+		.bs("Redoslijed / Prioritet")
+		.sr("Redoslijed / Prioritet")
+		.hr("Redoslijed / Prioritet");
 	I18n0 POSITION = new I18n0("Position")//
-		.de("Position");
+		.de("Position")
+		.bs("Pozicija")
+		.sr("Pozicija")
+		.hr("Pozicija");
 	I18n0 REMOVE_FILTER = new I18n0("Remove Filter")//
-		.de("Filter entfernen");
+		.de("Filter entfernen")
+		.bs("Ukloniti filter")
+		.sr("Ukloniti filter")
+		.hr("Ukloniti filter");
 	I18n0 REMOVE_FILTERS_FROM_OTHER_COLUMNS = new I18n0("Remove filters from other columns")//
-		.de("Filter von anderen Spalten entfernen");
+		.de("Filter von anderen Spalten entfernen")
+		.bs("Uklonite filtre iz drugih kolona")
+		.sr("Uklonite filtre iz drugih kolona")
+		.hr("Uklonite filtre iz drugih kolona");
 	I18n0 RESET = new I18n0("Reset")//
-		.de("Zurücksetzen");
+		.de("Zurücksetzen")
+		.bs("Resetirati")
+		.sr("Resetirati")
+		.hr("Resetirati");
 	I18n0 ROWS_PER_PAGE = new I18n0("Rows per Page")//
-		.de("Zeilen pro Seite");
+		.de("Zeilen pro Seite")
+		.bs("Redovi po stranici")
+		.sr("Redovi po stranici")
+		.hr("Redovi po stranici");
 	I18n0 SELECTION = new I18n0("Selection")//
-		.de("Auswahl");
+		.de("Auswahl")
+		.bs("Izbor")
+		.sr("Izbor")
+		.hr("Izbor");
 	I18n1 SORT_VALUES_OF_COLUMN_ARG1_IN_ASCENDING_ORDER = new I18n1("Sort values of column '%s' in ascending order.")//
-		.de("Sortiere Werte in Spalte '%s' in aufsteigender Reihenfolge.");
+		.de("Sortiere Werte in Spalte '%s' in aufsteigender Reihenfolge.")
+		.bs("Poredati vrijednosti kolona '%s' uzlaznim redoslijedom.")
+		.sr("Poredati vrednosti kolona '%s' uzlaznim redoslijedom.")
+		.hr("Poredati vrijednosti kolona '%s' uzlaznim redoslijedom.");
 	I18n1 SORT_VALUES_OF_COLUMN_ARG1_IN_DESCENDING_ORDER = new I18n1("Sort values of column '%s' in descending order.")//
-		.de("Sortiere Werte in Spalte '%s' in absteigender Reihenfolge.");
+		.de("Sortiere Werte in Spalte '%s' in absteigender Reihenfolge.")
+		.bs("Poredati vrijednosti kolona '%s' silaznim redoslijedom.")
+		.sr("Poredati vrednosti kolona '%s' silaznim redoslijedom.")
+		.hr("Poredati vrijednosti kolona '%s' silaznim redoslijedom.");
 	I18n0 TABLE_CONFIGURATION = new I18n0("Table Configuration")//
-		.de("Tabellen-Konfiguration");
+		.de("Tabellen-Konfiguration")
+		.bs("Konfiguracija tabele")
+		.sr("Konfiguracija tabele")
+		.hr("Konfiguracija tabele");
 	I18n0 YES = new I18n0("Yes")//
-		.de("Ja");
+		.de("Ja")
+		.bs("Da")
+		.sr("Da")
+		.hr("Da");
 }
