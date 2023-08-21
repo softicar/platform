7390856a27a3e6b50f66df8e5bd0d3b08231aa42
Daniel Klose <daniel.klose@forspace-solutions.com>
2023-08-21 17:29:22 +0200

    added assert in test

--- platform-core-module/src/test/java/com/softicar/platform/core/module/page/service/login/PageServiceLoginDivTest.java
+++ platform-core-module/src/test/java/com/softicar/platform/core/module/page/service/login/PageServiceLoginDivTest.java
@@ -181,7 +181,7 @@ public class PageServiceLoginDivTest extends AbstractDbTest implements IAjaxSele
 		send(findModalAlertOrFail().getCloseButton(), Key.ENTER);
 
 		//TODO this somehow fails in github but not locally
-		//assertEquals(1, AGUserPasswordResetRequest.TABLE.countAll());
+		assertEquals(1, AGUserPasswordResetRequest.TABLE.countAll());
 	}
 
 	@Test
