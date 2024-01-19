7f4ae27924f1f7de29811769e9cb70db1a0febbb
Thees Koester <thees.koester@forspace-solutions.com>
2024-01-19 11:24:43 +0100

    EXP-3130: Fix OutOfMemoryError in ProgramExecutionPage (#860)

--- platform-core-module/src/main/java/com/softicar/platform/core/module/program/execution/ProgramExecutionPage.java
+++ platform-core-module/src/main/java/com/softicar/platform/core/module/program/execution/ProgramExecutionPage.java
@@ -1,6 +1,7 @@
 package com.softicar.platform.core.module.program.execution;
 
 import com.softicar.platform.common.code.reference.point.SourceCodeReferencePointUuid;
+import com.softicar.platform.common.core.i18n.IDisplayString;
 import com.softicar.platform.core.module.AGCoreModuleInstance;
 import com.softicar.platform.core.module.CoreI18n;
 import com.softicar.platform.core.module.CoreModule;
@@ -8,13 +9,12 @@ import com.softicar.platform.core.module.CorePermissions;
 import com.softicar.platform.core.module.program.ProgramMaintenanceMessageBar;
 import com.softicar.platform.dom.elements.DomDiv;
 import com.softicar.platform.dom.node.IDomNode;
-import com.softicar.platform.emf.management.page.AbstractEmfManagementPage;
 import com.softicar.platform.emf.module.permission.IEmfModulePermission;
 import com.softicar.platform.emf.page.EmfPagePath;
-import com.softicar.platform.emf.table.IEmfTable;
+import com.softicar.platform.emf.page.IEmfPage;
 
 @SourceCodeReferencePointUuid("5ff82872-cd52-46ec-a76a-f35ca142322a")
-public class ProgramExecutionPage extends AbstractEmfManagementPage<AGCoreModuleInstance> {
+public class ProgramExecutionPage implements IEmfPage<AGCoreModuleInstance> {
 
 	@Override
 	public Class<CoreModule> getModuleClass() {
@@ -23,9 +23,9 @@ public class ProgramExecutionPage extends AbstractEmfManagementPage<AGCoreModule
 	}
 
 	@Override
-	protected IEmfTable<?, ?, AGCoreModuleInstance> getTable() {
+	public IDisplayString getTitle(AGCoreModuleInstance moduleInstance) {
 
-		return AGProgramExecution.TABLE;
+		return CoreI18n.PROGRAM_EXECUTIONS;
 	}
 
 	@Override
@@ -43,9 +43,9 @@ public class ProgramExecutionPage extends AbstractEmfManagementPage<AGCoreModule
 	@Override
 	public IDomNode createContentNode(AGCoreModuleInstance moduleInstance) {
 
-		var div = new DomDiv();
+		DomDiv div = new DomDiv();
 		div.appendChild(new ProgramMaintenanceMessageBar());
-		div.appendChild(super.createContentNode(moduleInstance));
+		div.appendChild(new ProgramExecutionsDiv());
 		return div;
 	}
 }
